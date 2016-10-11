6234a7f01f07fdde03b76c25f9177134aa8b2e27
ajv <voisine@gmail.com>
2016-10-11 01:47:17 -0700

    connects to bitcoin network

--- breadwallet/ViewController.swift
+++ breadwallet/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import BRCore
-import libunbound
+//import libunbound
 
 class ViewController: UIViewController {
 
@@ -16,20 +16,66 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var key = BRKey()
-        var secret = UInt256(u64: (0, 0, 0, 1))
-        let mpk = BRMasterPubKey()
-        var tx = BRTransactionNew()
-        let wallet = BRWalletNew(&tx, 0, mpk)
+        var seed = [UInt8](repeating: 0x00, count: 512/8)
         
-        BRKeySetSecret(&key, &secret, 0)
-        
-        BRWalletFree(wallet)
-        BRTransactionFree(tx)
+        //BRBIP39DeriveKey(&seed, "video tiger report bid suspect taxi mail argue naive layer metal surface", nil);
+        BRBIP39DeriveKey(&seed, "axis husband project any sea patch drip tip spirit tide bring belt", nil);
 
-        let ctx = ub_ctx_create()
+        let mpk = BRBIP32MasterPubKey(&seed, 512/8);
+        let wallet = BRWalletNew(nil, 0, mpk);
         
-        ub_ctx_delete(ctx)
+        BRWalletSetCallbacks(wallet, nil,
+            { (info, balance) in // balanceChanged
+            },
+            { (info, tx) in // txAdded
+            },
+            { (info, txHashes, txCount, blockHeight, timestamp) in // txUpdated
+            },
+            { (info, txHash, notifyUser, recommendRescan) in // txDeleted
+            }
+        )
+
+        var addr = BRWalletReceiveAddress(wallet)
+        
+        print("wallet created with first receive address:", withUnsafePointer(to: &addr.s) {
+            $0.withMemoryRebound(to: CChar.self, capacity: MemoryLayout<BRAddress>.stride) { String(cString:$0) }
+        })
+        
+        let manager = BRPeerManagerNew(wallet, UInt32(BIP39_CREATION_TIME), nil, 0, nil, 0);
+
+        BRPeerManagerSetCallbacks(manager, nil,
+            { (info) in // syncStarted
+                print("sync started")
+            },
+            { (info) in // syncSucceeded
+                print("sync succeeded")
+            },
+            { (info, error) in // syncFailed
+                print("sync failed: ", strerror(error))
+            },
+            { (info) in // txStatusUpdate
+            },
+            { (info, blocks, blockCount) in // saveBlocks
+            },
+            { (info, peers, peerCount) in // savePeers
+            },
+            { (info) -> Int32 in // networkIsReachable
+                return 1
+            },
+            { (info) in // threadCleanup
+            }
+        )
+        
+        BRPeerManagerConnect(manager);
+        
+//        BRPeerManagerDisconnect(manager);
+//        BRPeerManagerFree(manager);
+//        BRWalletFree(wallet);
+
+//        let ctx = ub_ctx_create()
+//
+//        ub_ctx_delete(ctx)
+
     }
 
     override func didReceiveMemoryWarning() {
