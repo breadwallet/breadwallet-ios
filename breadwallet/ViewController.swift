646c9a2bcff537b9f3f85550bc3d8596fc466091
ajv <voisine@gmail.com>
2016-10-18 18:03:47 -0700

    sqlite3 module

--- breadwallet/ViewController.swift
+++ breadwallet/ViewController.swift
@@ -2,80 +2,34 @@
 //  ViewController.swift
 //  breadwallet
 //
-//  Created by ajv on 10/5/16.
-//  Copyright © 2016 breadwallet LLC. All rights reserved.
+//  Created by Aaron Voisine on 10/5/16.
+//  Copyright (c) 2016 breadwallet LLC
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 
 import UIKit
-import BRCore
-//import libunbound
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        var seed = [UInt8](repeating: 0x00, count: 512/8)
-        
-        //BRBIP39DeriveKey(&seed, "video tiger report bid suspect taxi mail argue naive layer metal surface", nil);
-        BRBIP39DeriveKey(&seed, "axis husband project any sea patch drip tip spirit tide bring belt", nil);
-
-        let mpk = BRBIP32MasterPubKey(&seed, 512/8);
-        let wallet = BRWalletNew(nil, 0, mpk);
-        
-        BRWalletSetCallbacks(wallet, nil,
-            { (info, balance) in // balanceChanged
-            },
-            { (info, tx) in // txAdded
-            },
-            { (info, txHashes, txCount, blockHeight, timestamp) in // txUpdated
-            },
-            { (info, txHash, notifyUser, recommendRescan) in // txDeleted
-            }
-        )
-
-        var addr = BRWalletReceiveAddress(wallet)
-        
-        print("wallet created with first receive address:", withUnsafePointer(to: &addr.s) { ptr -> String in
-            return String(cString: UnsafeRawPointer(ptr).assumingMemoryBound(to: CChar.self))
-        })
-        
-        let manager = BRPeerManagerNew(wallet, UInt32(BIP39_CREATION_TIME), nil, 0, nil, 0);
-
-        BRPeerManagerSetCallbacks(manager, nil,
-            { (info) in // syncStarted
-                print("sync started")
-            },
-            { (info) in // syncSucceeded
-                print("sync succeeded")
-            },
-            { (info, error) in // syncFailed
-                print("sync failed:", String(cString:strerror(error)))
-            },
-            { (info) in // txStatusUpdate
-            },
-            { (info, blocks, blockCount) in // saveBlocks
-            },
-            { (info, peers, peerCount) in // savePeers
-            },
-            { (info) -> Int32 in // networkIsReachable
-                return 1
-            },
-            { (info) in // threadCleanup
-            }
-        )
-        
-        BRPeerManagerConnect(manager);
-        
-//        BRPeerManagerDisconnect(manager);
-//        BRPeerManagerFree(manager);
-//        BRWalletFree(wallet);
-
-//        let ctx = ub_ctx_create()
-//
-//        ub_ctx_delete(ctx)
-
     }
 
     override func didReceiveMemoryWarning() {
