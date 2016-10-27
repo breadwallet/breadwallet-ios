97f8f34a00f8ff1e23fe7318ad88462d4f688619
Adrian Corscadden <me@acorscadden.com>
2016-10-27 16:48:24 -0700

    Add confirm phrase text fields

--- breadwallet/src/ViewControllers/ConfirmPaperPhraseViewController.swift
+++ breadwallet/src/ViewControllers/ConfirmPaperPhraseViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ConfirmPaperPhraseViewController: UIViewController {
 
+    private let label =                 UILabel.makeWrappingLabel(font: UIFont.preferredFont(forTextStyle: .body))
+    private let separator =             UIView()
+    private let confirmFirstPhrase =    ConfirmPhrase(text: "Word 3")
+    private let confirmSecondPhrase =   ConfirmPhrase(text: "Word 8")
+    
     private let store: Store
 
     init(store: Store) {
@@ -19,17 +24,37 @@ class ConfirmPaperPhraseViewController: UIViewController {
 
     override func viewDidLoad() {
         view.backgroundColor = .white
+        label.text = "Prove you wrote down your paper key by answering the following questions."
+        separator.backgroundColor = .darkGray
         addSubviews()
         addConstraints()
         addButtonActions()
     }
 
     private func addSubviews() {
-
+        view.addSubview(label)
+        view.addSubview(separator)
+        view.addSubview(confirmFirstPhrase)
+        view.addSubview(confirmSecondPhrase)
     }
 
     private func addConstraints() {
-
+        label.constrainTopCorners(sidePadding: Constants.Padding.double, topPadding: Constants.Padding.triple, topLayoutGuide: topLayoutGuide)
+        separator.constrain([
+                separator.constraint(toBottom: label, constant: Constants.Padding.double),
+                separator.constraint(.height, constant: 1.0),
+                separator.constraint(.width, toView: view, constant: 0.0)
+            ])
+        confirmFirstPhrase.constrain([
+                confirmFirstPhrase.constraint(toBottom: separator, constant: 0.0),
+                confirmFirstPhrase.constraint(.width, toView: view, constant: 0.0),
+                confirmFirstPhrase.constraint(.centerX, toView: view, constant: 0.0)
+            ])
+        confirmSecondPhrase.constrain([
+                confirmSecondPhrase.constraint(toBottom: confirmFirstPhrase, constant: 0.0),
+                confirmSecondPhrase.constraint(.width, toView: view, constant: 0.0),
+                confirmSecondPhrase.constraint(.centerX, toView: view, constant: 0.0)
+            ])
     }
 
     private func addButtonActions() {
---
+++ breadwallet/src/Views/ConfirmPhrase.swift
@@ -0,0 +1,53 @@
+//
+//  ConfirmPhrase.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2016-10-27.
+//  Copyright © 2016 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+
+class ConfirmPhrase: UIView {
+
+    private let label =     UILabel()
+    private let textField = UITextField()
+    private let separator = UIView()
+
+    init(text: String) {
+        super.init(frame: CGRect())
+        label.text = text
+        separator.backgroundColor = .darkGray
+        setupSubviews()
+    }
+
+    private func setupSubviews() {
+        addSubview(label)
+        addSubview(textField)
+        addSubview(separator)
+
+        label.constrain([
+                label.constraint(.leading, toView: self, constant: Constants.Padding.single),
+                label.constraint(.top, toView: self, constant: Constants.Padding.quad)
+            ])
+        textField.constrain([
+                textField.constraint(.leading, toView: label, constant: nil),
+                textField.constraint(toBottom: label, constant: Constants.Padding.half),
+                textField.constraint(.width, toView: self, constant: -Constants.Padding.single*2)
+            ])
+
+        separator.constrainBottomCorners(sidePadding: 0.0, bottomPadding: 0.0)
+        separator.constrain([
+                //This contraint to the bottom of the textField is pretty crucial. Without it,
+                //this view will have an intrinsicHeight of 0
+                separator.constraint(toBottom: textField, constant: Constants.Padding.single),
+                separator.constraint(.height, constant: 1.0)
+            ])
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
