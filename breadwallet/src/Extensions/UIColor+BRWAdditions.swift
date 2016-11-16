e3f52bc54b15630f6c0d702d22f64efe5b54d1a8
Adrian Corscadden <me@acorscadden.com>
2016-11-15 16:26:10 -0800

    Implement primary and secondary button colors

--- breadwallet/src/Extensions/UIColor+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIColor+BRWAdditions.swift
@@ -14,6 +14,22 @@ extension UIColor {
         return UIColor(red: 76.0/255.0, green: 152.0/255.0, blue: 252.0/255.0, alpha: 1.0)
     }
 
+    static var primaryText: UIColor {
+        return .white
+    }
+
+    static var secondaryButton: UIColor {
+        return UIColor(red: 245.0/255.0, green: 247.0/255.0, blue: 250.0/255.0, alpha: 1.0)
+    }
+
+    static var secondaryBorder: UIColor {
+        return UIColor(red: 213.0/255.0, green: 218.0/255.0, blue: 224.0/255.0, alpha: 1.0)
+    }
+
+    static var secondaryText: UIColor {
+        return UIColor(red: 35.0/255.0, green: 37.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+    }
+
     static var brand: UIColor {
         return UIColor(red: 0.0, green: 197.0/255.0, blue: 167.0/255.0, alpha: 1.0)
     }
--- breadwallet/src/Views/ShadowButton.swift
+++ breadwallet/src/Views/ShadowButton.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+enum ButtonType {
+    case primary
+    case secondary
+}
+
 class ShadowButton: UIControl {
 
     private let title: String
+    private let type: ButtonType
     private let container = UIView()
     private let shadowView = UIView()
+    private let label = UILabel()
     private let shadowSidePadding: CGFloat = 32.0
     private let shadowYOffset: CGFloat = 4.0
 
@@ -32,8 +39,9 @@ class ShadowButton: UIControl {
         }
     }
 
-    init(title: String) {
+    init(title: String, type: ButtonType) {
         self.title = title
+        self.type = type
         super.init(frame: CGRect.zero)
         setupViews()
     }
@@ -41,6 +49,7 @@ class ShadowButton: UIControl {
     private func setupViews() {
         addShadowView()
         addContent()
+        setColors()
     }
 
     private func addShadowView() {
@@ -51,7 +60,7 @@ class ShadowButton: UIControl {
             ])
         shadowView.layer.cornerRadius = 4.0
         shadowView.layer.shadowColor = UIColor.black.cgColor
-        shadowView.layer.shadowOpacity = 0.5
+        shadowView.layer.shadowOpacity = 0.3
         shadowView.layer.shadowOffset = CGSize(width: 0, height: 4)
         shadowView.backgroundColor = .white
         shadowView.isUserInteractionEnabled = false
@@ -64,15 +73,27 @@ class ShadowButton: UIControl {
         container.isUserInteractionEnabled = false
         container.constrain(toSuperviewEdges: nil)
 
-        let label = UILabel()
         container.addSubview(label)
         label.constrain(toSuperviewEdges: nil)
         label.text = title
         label.textColor = .white
         label.textAlignment = .center
         label.isUserInteractionEnabled = false
+        label.font = UIFont.customMedium(size: 16.0)
     }
 
+    private func setColors() {
+        switch type {
+            case .primary:
+                container.backgroundColor = .primaryButton
+                label.textColor = .primaryText
+            case .secondary:
+                container.backgroundColor = .secondaryButton
+                label.textColor = .secondaryText
+                container.layer.borderColor = UIColor.secondaryBorder.cgColor
+                container.layer.borderWidth = 1.0
+        }
+    }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
