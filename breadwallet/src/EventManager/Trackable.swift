683178542756c2967e092fe28da8d5c8b0120c13
Adrian Corscadden <me@acorscadden.com>
2016-11-20 22:53:57 -0800

    First pass at Event Manager Swift port

--- breadwallet/src/EventManager/Trackable.swift
+++ breadwallet/src/EventManager/Trackable.swift
@@ -2,7 +2,7 @@
 //  Trackable.swift
 //  breadwallet
 //
-//  Created by Adrian Corscadden on 2016-11-19.
+//  Created by Adrian Corscadden on 2016-11-20.
 //  Copyright © 2016 breadwallet LLC. All rights reserved.
 //
 
@@ -11,26 +11,4 @@ import Foundation
 protocol Trackable {
     func saveEvent(_ eventName: String)
     func saveEvent(_ eventName: String, attributes: [String: String])
-}
-
-extension Trackable {
-    func saveEvent(_ eventName: String) {
-        EventManager.shared.saveEvent(eventName)
-    }
-
-    func saveEvent(_ eventName: String, attributes: [String: String]) {
-        EventManager.shared.saveEvent(eventName, attributes: attributes)
-    }
-}
-
-fileprivate class EventManager {
-    static let shared = EventManager()
-
-    func saveEvent(_ eventName: String) {
-
-    }
-
-    func saveEvent(_ eventName: String, attributes: [String: String]) {
-
-    }
 }
