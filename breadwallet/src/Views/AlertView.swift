aab4825ee5eb04573cec740017ab4d0f55e18e45
Adrian Corscadden <me@acorscadden.com>
2016-11-23 08:18:01 -0800

    Add missing Alert view subviews

--- breadwallet/src/Views/AlertView.swift
+++ breadwallet/src/Views/AlertView.swift
@@ -37,10 +37,13 @@ enum AlertType {
 
 class AlertView: UIView, GradientDrawable {
 
-    let type: AlertType
-    let header = UILabel()
-    let subheader = UILabel()
-    var icon: UIView
+    private let type: AlertType
+    private let header = UILabel()
+    private let subheader = UILabel()
+    private let separator = UIView()
+    private let icon: UIView
+    private let iconSize: CGFloat = 96.0
+    private let separatorYOffset: CGFloat = 48.0
 
     init(type: AlertType) {
         self.type = type
@@ -61,20 +64,51 @@ class AlertView: UIView, GradientDrawable {
         addSubview(header)
         addSubview(subheader)
         addSubview(icon)
+        addSubview(separator)
 
-        header.constrainTopCorners(sidePadding: C.padding[2], topPadding: C.padding[4])
+        setData()
+        addConstraints()
+    }
 
+    private func setData() {
         header.text = type.header
         header.textAlignment = .center
         header.font = UIFont.customBold(size: 14.0)
         header.textColor = .white
 
         icon.backgroundColor = .clear
+        separator.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
+
+        subheader.text = type.subheader
+        subheader.textAlignment = .center
+        subheader.font = UIFont.customBody(size: 14.0)
+        subheader.textColor = .white
+    }
+
+    private func addConstraints() {
+
+        //NB - In this alert view, constraints shouldn't be pinned to the bottom
+        //of the view because the bottom actually extends off the bottom of the screen a bit.
+        //It extends so that it still covers up the underlying view when it bounces on screen.
+
+        header.constrainTopCorners(sidePadding: C.padding[2], topPadding: C.padding[2])
+        separator.constrain([
+            separator.constraint(.height, constant: 1.0),
+            separator.constraint(.width, toView: self, constant: 0.0),
+            separator.constraint(.top, toView: self, constant: separatorYOffset),
+            separator.constraint(.leading, toView: self, constant: nil)
+            ])
         icon.constrain([
                 icon.constraint(.centerX, toView: self, constant: nil),
                 icon.constraint(.centerY, toView: self, constant: nil),
-                icon.constraint(.width, constant: 96.0),
-                icon.constraint(.height, constant: 96.0)
+                icon.constraint(.width, constant: iconSize),
+                icon.constraint(.height, constant: iconSize)
+            ])
+
+        subheader.constrain([
+                subheader.constraint(.leading, toView: self, constant: C.padding[2]),
+                subheader.constraint(.trailing, toView: self, constant: -C.padding[2]),
+                subheader.constraint(toBottom: icon, constant: C.padding[3])
             ])
     }
 
