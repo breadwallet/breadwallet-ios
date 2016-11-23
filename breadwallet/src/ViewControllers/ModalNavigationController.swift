144007b0919a31c1945333b019844d8e4e553f84
Adrian Corscadden <me@acorscadden.com>
2016-11-23 09:00:53 -0800

    Switch to UIViewController based status bar style

---
+++ breadwallet/src/ViewControllers/ModalNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  ModalNavigationController.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2016-11-23.
+//  Copyright © 2016 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class ModalNavigationController: UINavigationController {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .default
+    }
+}
