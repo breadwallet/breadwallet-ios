854383eff901554c701286dfb9bd2e62d35401d3
Adrian Corscadden <me@acorscadden.com>
2016-12-12 09:51:27 -0800

    Add initial scan view controller

---
+++ breadwallet/src/ScanViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraViewController.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2016-12-12.
+//  Copyright © 2016 breadwallet LLC. All rights reserved.
+//
+
+import Foundation
