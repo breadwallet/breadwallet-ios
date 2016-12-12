c821fcdebf4e1bb641e8e1df344b9163e92dc9b2
Adrian Corscadden <me@acorscadden.com>
2016-12-11 18:11:08 -0800

    Make start flow animations work on < iOS 10

--- breadwallet/src/Extensions/UIViewPropertyAnimator+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIViewPropertyAnimator+BRWAdditions.swift
@@ -11,19 +11,15 @@ import UIKit
 @available(iOS 10.0, *)
 extension UIViewPropertyAnimator {
 
-    static func springAnimation(_ animations: @escaping ()->Void) -> UIViewPropertyAnimator {
-        let springParameters = UISpringTimingParameters(dampingRatio: 0.7)
-        let animator = UIViewPropertyAnimator(duration: 0.6, timingParameters: springParameters)
-        animator.addAnimations(animations)
-        return animator
-    }
-
-    static func springAnimation(_ duration: TimeInterval, animations: @escaping () -> Void, completion: @escaping (UIViewAnimatingPosition) -> Void) {
+    static func springAnimation(_ duration: TimeInterval, delay: TimeInterval, animations: @escaping () -> Void, completion: @escaping (UIViewAnimatingPosition) -> Void) {
         let springParameters = UISpringTimingParameters(dampingRatio: 0.7)
         let animator = UIViewPropertyAnimator(duration: duration, timingParameters: springParameters)
         animator.addAnimations(animations)
         animator.addCompletion(completion)
-        animator.startAnimation()
+        animator.startAnimation(afterDelay: delay)
     }
 
+    static func springAnimation(_ duration: TimeInterval, animations: @escaping () -> Void, completion: @escaping (UIViewAnimatingPosition) -> Void) {
+        springAnimation(duration, delay: 0.0, animations: animations, completion: completion)
+    }
 }
