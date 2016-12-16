0288f2a43211b4da2dd203223aca89e1fe35c613
Adrian Corscadden <me@acorscadden.com>
2016-12-16 09:51:15 -0800

    Add textfield to amount cell

--- breadwallet/src/Views/SendCell.swift
+++ breadwallet/src/Views/SendCell.swift
@@ -8,7 +8,20 @@
 
 import UIKit
 
-class SendCell: UIView {
+class SendCell : UIView {
+
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        let attributes: [String: Any] = [
+            NSForegroundColorAttributeName: UIColor.grayTextTint,
+            NSFontAttributeName : placeholderFont
+        ]
+        textField.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: attributes)
+        textField.tintColor = C.defaultTintColor
+        textField.delegate = self
+        textField.textColor = .darkText
+        setupViews()
+    }
 
     init(label: String) {
         super.init(frame: .zero)
@@ -18,35 +31,49 @@ class SendCell: UIView {
 
     var content: String? {
         didSet {
-            contentLabel.text = content
+            if textField.placeholder != nil {
+                textField.text = content
+                guard let count = content?.characters.count else { return }
+                textField.font = count > 0 ? textFieldFont : placeholderFont
+            } else {
+                contentLabel.text = content
+            }
         }
     }
 
+    var textFieldDidBeginEditing: (() -> Void)?
+
     let accessoryView = UIView()
 
+    fileprivate let placeholderFont = UIFont.customBody(size: 16.0)
+    fileprivate let textFieldFont = UIFont.customBody(size: 26.0)
+
     private let label = UILabel(font: .customBody(size: 16.0))
+    private let textField = UITextField()
     private let contentLabel = UILabel(font: .customBody(size: 14.0))
     private let border = UIView()
 
     private func setupViews() {
         addSubview(label)
+        addSubview(textField)
         addSubview(contentLabel)
         addSubview(accessoryView)
         addSubview(border)
         label.constrain([
-                label.constraint(.centerY, toView: self),
-                label.constraint(.leading, toView: self, constant: C.padding[2])
-            ])
+            label.constraint(.centerY, toView: self),
+            label.constraint(.leading, toView: self, constant: C.padding[2]) ])
+        textField.constrain([
+            textField.constraint(.leading, toView: self, constant: C.padding[2]),
+            textField.constraint(.top, toView: self),
+            textField.constraint(.bottom, toView: self) ])
         contentLabel.constrain([
-                contentLabel.constraint(.leading, toView: label),
-                contentLabel.constraint(toBottom: label, constant: 0.0),
-                contentLabel.constraint(toLeading: accessoryView, constant: -C.padding[2])
-            ])
+            contentLabel.constraint(.leading, toView: label),
+            contentLabel.constraint(toBottom: label, constant: 0.0),
+            contentLabel.constraint(toLeading: accessoryView, constant: -C.padding[2]) ])
         accessoryView.constrain([
-                accessoryView.constraint(.top, toView: self),
-                accessoryView.constraint(.trailing, toView: self),
-                accessoryView.constraint(.bottom, toView: self)
-            ])
+            accessoryView.constraint(.top, toView: self),
+            accessoryView.constraint(.trailing, toView: self),
+            accessoryView.constraint(.bottom, toView: self) ])
         border.constrainBottomCorners(height: 1.0)
 
         border.backgroundColor = .secondaryShadow
@@ -56,5 +83,11 @@ class SendCell: UIView {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SendCell : UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textFieldDidBeginEditing?()
     }
 }
