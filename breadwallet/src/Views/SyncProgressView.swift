4896e4c36ab8ea2c65ee91be3c51377ce6d444bd
Adrian Corscadden <me@acorscadden.com>
2017-01-08 18:44:51 -0800

    Implement sync progress view design

--- breadwallet/src/Views/SyncProgressView.swift
+++ breadwallet/src/Views/SyncProgressView.swift
@@ -8,16 +8,63 @@
 
 import UIKit
 
+private let progressHeight: CGFloat = 4.0
+private let progressWidth: CGFloat = 150.0
+
 class SyncProgressView : UIView, GradientDrawable {
 
-    let label = UILabel()
+    var progress: Double = 0.0 {
+        didSet {
+            progressWidthConstraint?.constant = CGFloat(progress)*progressWidth
+        }
+    }
 
     init() {
         super.init(frame: .zero)
-        addTopCorners()
+        setupView()
+    }
+
+    lazy private var progressBackground: UIView = self.makeProgressView(backgroundColor: UIColor(white: 0.0, alpha: 0.3))
+    lazy private var progressForeground: UIView = self.makeProgressView(backgroundColor: .white)
+
+    private func makeProgressView(backgroundColor: UIColor) -> UIView {
+        let view = UIView()
+        view.layer.cornerRadius = progressHeight/2.0
+        view.layer.masksToBounds = true
+        view.backgroundColor = backgroundColor
+        return view
+    }
+
+    private let label = UILabel(font: .customBold(size: 14.0))
+    private let shadowView = UIView()
+    private var progressWidthConstraint: NSLayoutConstraint?
+
+    private func setupView() {
+        label.textColor = .white
+        label.text = S.Account.loadingMessage
+        label.textAlignment = .center
 
         addSubview(label)
-        label.constrain(toSuperviewEdges: nil)
+        addSubview(progressBackground)
+        addSubview(shadowView)
+
+        label.constrain([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.bottomAnchor.constraint(equalTo: progressBackground.topAnchor, constant: -4.0) ])
+        progressBackground.constrain([
+            progressBackground.centerXAnchor.constraint(equalTo: centerXAnchor),
+            progressBackground.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -C.padding[1]),
+            progressBackground.heightAnchor.constraint(equalToConstant: progressHeight),
+            progressBackground.widthAnchor.constraint(equalToConstant: progressWidth) ])
+        progressBackground.addSubview(progressForeground)
+        progressWidthConstraint = progressForeground.widthAnchor.constraint(equalToConstant: 0.0)
+        progressForeground.constrain([
+            progressWidthConstraint,
+            progressForeground.leadingAnchor.constraint(equalTo: progressBackground.leadingAnchor),
+            progressForeground.heightAnchor.constraint(equalTo: progressBackground.heightAnchor),
+            progressForeground.centerYAnchor.constraint(equalTo: progressBackground.centerYAnchor) ])
+        shadowView.backgroundColor = UIColor(white: 1.0, alpha: 0.3)
+        shadowView.constrainTopCorners(height: 0.5)
     }
 
     override func layoutSubviews() {
