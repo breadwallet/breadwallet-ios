526f28f41451228b0f1bd369141db6250c69e450
Adrian Corscadden <me@acorscadden.com>
2017-01-22 22:06:42 -0800

    Add login pidpad

--- breadwallet/src/ViewControllers/LoginViewController.swift
+++ breadwallet/src/ViewControllers/LoginViewController.swift
@@ -20,10 +20,31 @@ class LoginViewController : UIViewController {
     private let store: Store
     private let backgroundView = LoginBackgroundView()
     private let textField = UITextField()
+    private let pinPad = PinPadViewController()
+    private let pinView = PinView()
 
     override func viewDidLoad() {
         view.addSubview(backgroundView)
         backgroundView.constrain(toSuperviewEdges: nil)
+
+        addChildViewController(pinPad, layout: {
+            pinPad.view.constrainBottomCorners(sidePadding: 0.0, bottomPadding: 0.0)
+            pinPad.view.constrain([
+                pinPad.view.heightAnchor.constraint(equalToConstant: 216.0) ])
+        })
+
+        view.addSubview(pinView)
+        pinView.constrain([
+            pinView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pinView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            pinView.widthAnchor.constraint(equalToConstant: pinView.defaultWidth),
+            pinView.heightAnchor.constraint(equalToConstant: pinView.defaultPinSize) ])
+
+        pinPad.ouputDidUpdate = { pin in
+            let length = pin.lengthOfBytes(using: .utf8)
+            self.pinView.fill(length)
+            self.pinPad.isAppendingDisabled = length < 6 ? false : true
+        }
     }
 
     required init?(coder aDecoder: NSCoder) {
