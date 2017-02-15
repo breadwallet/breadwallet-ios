e0d3de39a88574a750e30debd6f1a4d7efa23ac9
Adrian Corscadden <me@acorscadden.com>
2017-02-15 08:55:47 -0800

    Add security center stretchy header

--- breadwallet/src/ViewControllers/SecurityCenterViewController.swift
+++ breadwallet/src/ViewControllers/SecurityCenterViewController.swift
@@ -14,25 +14,55 @@ class SecurityCenterHeader : UIView, GradientDrawable {
     }
 }
 
+private let headerHeight: CGFloat = 222.0
+
 class SecurityCenterViewController : UIViewController {
 
     override func viewDidLoad() {
         view.backgroundColor = .white
-        view.addSubview(headerBackground)
-        view.addSubview(header)
-        view.addSubview(shield)
-        headerBackground.constrainTopCorners(sidePadding: 0.0, topPadding: 0.0)
+        view.addSubview(scrollView)
+
+        scrollView.addSubview(headerBackground)
+        headerBackground.addSubview(header)
+        headerBackground.addSubview(shield)
+
+        scrollView.constrain([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor) ])
+
         headerBackground.constrain([
-            headerBackground.heightAnchor.constraint(equalToConstant: 222.0) ])
-        header.constrainTopCorners(sidePadding: 0.0, topPadding: 0.0, topLayoutGuide: topLayoutGuide)
+            headerBackground.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            headerBackground.topAnchor.constraint(equalTo: view.topAnchor),
+            headerBackground.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            headerBackground.widthAnchor.constraint(equalTo: view.widthAnchor) ])
+        headerBackgroundHeight = headerBackground.heightAnchor.constraint(equalToConstant: headerHeight)
+        headerBackground.constrain([headerBackgroundHeight])
+
         header.constrain([
-            header.heightAnchor.constraint(equalToConstant: C.Sizes.headerHeight) ])
+            header.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            header.topAnchor.constraint(equalTo: headerBackground.topAnchor, constant: 20.0),
+            header.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            header.heightAnchor.constraint(equalToConstant: C.Sizes.headerHeight)])
         header.closeCallback = {
             self.dismiss(animated: true, completion: nil)
         }
+
         shield.constrain([
             shield.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            shield.topAnchor.constraint(equalTo: header.bottomAnchor, constant: C.padding[2]) ])
+            shield.centerYAnchor.constraint(equalTo: headerBackground.centerYAnchor, constant: C.padding[3]) ])
+
+        scrollView.addSubview(info)
+        scrollView.alwaysBounceVertical = true
+        scrollView.delegate = self
+        info.constrain([
+            info.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: C.padding[2]),
+            info.topAnchor.constraint(equalTo: headerBackground.bottomAnchor, constant: C.padding[2]),
+            info.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -C.padding[4]) ])
+        info.text = "Breadwallet provides security features for protecting your money. Click each feature below to learn more."
+        info.numberOfLines = 0
+        info.lineBreakMode = .byWordWrapping
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -41,5 +71,15 @@ class SecurityCenterViewController : UIViewController {
 
     private let headerBackground = SecurityCenterHeader()
     private let header = ModalHeaderView(title: S.SecurityCenter.title, isFaqHidden: false, style: .light)
-    private let shield = UIImageView(image: #imageLiteral(resourceName: "shield"))
+    fileprivate let shield = UIImageView(image: #imageLiteral(resourceName: "shield"))
+    private let scrollView = UIScrollView()
+    private let info = UILabel(font: .customBody(size: 16.0))
+    fileprivate var headerBackgroundHeight: NSLayoutConstraint?
+}
+
+extension SecurityCenterViewController : UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+        headerBackgroundHeight?.constant = headerHeight - yOffset
+    }
 }
