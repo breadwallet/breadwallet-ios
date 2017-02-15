1556d125dd028ebbb8fa54b49ecc1d8458350fc3
Adrian Corscadden <me@acorscadden.com>
2017-02-15 10:43:07 -0800

    Clean up Security Center

--- breadwallet/src/ViewControllers/SecurityCenterViewController.swift
+++ breadwallet/src/ViewControllers/SecurityCenterViewController.swift
@@ -19,19 +19,43 @@ private let headerHeight: CGFloat = 222.0
 class SecurityCenterViewController : UIViewController {
 
     override func viewDidLoad() {
-        view.backgroundColor = .white
-        view.addSubview(scrollView)
+        setupSubviewProperties()
+        addSubviews()
+        addConstraints()
+    }
 
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    private func setupSubviewProperties() {
+        view.backgroundColor = .white
+        header.closeCallback = {
+            self.dismiss(animated: true, completion: nil)
+        }
+        scrollView.alwaysBounceVertical = true
+        scrollView.delegate = self
+        info.text = S.SecurityCenter.info
+        info.numberOfLines = 0
+        info.lineBreakMode = .byWordWrapping
+    }
+
+    private func addSubviews() {
+        view.addSubview(scrollView)
         scrollView.addSubview(headerBackground)
         headerBackground.addSubview(header)
         headerBackground.addSubview(shield)
+        scrollView.addSubview(pinCell)
+        scrollView.addSubview(touchIdCell)
+        scrollView.addSubview(paperKeyCell)
+        scrollView.addSubview(info)
+    }
 
+    private func addConstraints() {
         scrollView.constrain([
             scrollView.topAnchor.constraint(equalTo: view.topAnchor),
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor) ])
-
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),    scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor) ])
         headerBackground.constrain([
             headerBackground.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             headerBackground.topAnchor.constraint(equalTo: view.topAnchor),
@@ -39,46 +63,24 @@ class SecurityCenterViewController : UIViewController {
             headerBackground.widthAnchor.constraint(equalTo: view.widthAnchor) ])
         headerBackgroundHeight = headerBackground.heightAnchor.constraint(equalToConstant: headerHeight)
         headerBackground.constrain([headerBackgroundHeight])
-
         header.constrain([
             header.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             header.topAnchor.constraint(equalTo: headerBackground.topAnchor, constant: 20.0),
             header.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             header.heightAnchor.constraint(equalToConstant: C.Sizes.headerHeight)])
-        header.closeCallback = {
-            self.dismiss(animated: true, completion: nil)
-        }
-
         shield.constrain([
             shield.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             shield.centerYAnchor.constraint(equalTo: headerBackground.centerYAnchor, constant: C.padding[3]) ])
-
-        scrollView.addSubview(info)
-        scrollView.alwaysBounceVertical = true
-        scrollView.delegate = self
         info.constrain([
             info.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: C.padding[2]),
             info.topAnchor.constraint(equalTo: headerBackground.bottomAnchor, constant: C.padding[2]),
             info.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -C.padding[4]) ])
-        info.text = "Breadwallet provides security features for protecting your money. Click each feature below to learn more."
-        info.numberOfLines = 0
-        info.lineBreakMode = .byWordWrapping
-
-        let separator = UIView(color: .secondaryShadow)
         scrollView.addSubview(separator)
         separator.constrain([
             separator.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: C.padding[2]),
             separator.topAnchor.constraint(equalTo: info.bottomAnchor, constant: C.padding[2]),
             separator.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -C.padding[2]),
             separator.heightAnchor.constraint(equalToConstant: 1.0) ])
-
-        let pinCell = SecurityCenterCell(title: S.SecurityCenter.Cells.pinTitle, descriptionText: S.SecurityCenter.Cells.pinDescription)
-        pinCell.isEnabled = true
-        let touchIdCell = SecurityCenterCell(title: S.SecurityCenter.Cells.touchIdTitle, descriptionText: S.SecurityCenter.Cells.touchIdDescription)
-        let paperKeyCell = SecurityCenterCell(title: S.SecurityCenter.Cells.paperKeyTitle, descriptionText: S.SecurityCenter.Cells.paperKeyDescription)
-        scrollView.addSubview(pinCell)
-        scrollView.addSubview(touchIdCell)
-        scrollView.addSubview(paperKeyCell)
         pinCell.constrain([
             pinCell.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
             pinCell.topAnchor.constraint(equalTo: separator.bottomAnchor, constant: C.padding[2]),
@@ -94,16 +96,16 @@ class SecurityCenterViewController : UIViewController {
             paperKeyCell.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -C.padding[2]) ])
     }
 
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
-    }
-
+    fileprivate var headerBackgroundHeight: NSLayoutConstraint?
     private let headerBackground = SecurityCenterHeader()
     private let header = ModalHeaderView(title: S.SecurityCenter.title, isFaqHidden: false, style: .light)
-    fileprivate let shield = UIImageView(image: #imageLiteral(resourceName: "shield"))
+    private let shield = UIImageView(image: #imageLiteral(resourceName: "shield"))
     private let scrollView = UIScrollView()
     private let info = UILabel(font: .customBody(size: 16.0))
-    fileprivate var headerBackgroundHeight: NSLayoutConstraint?
+    private let pinCell = SecurityCenterCell(title: S.SecurityCenter.Cells.pinTitle, descriptionText: S.SecurityCenter.Cells.pinDescription)
+    private let touchIdCell = SecurityCenterCell(title: S.SecurityCenter.Cells.touchIdTitle, descriptionText: S.SecurityCenter.Cells.touchIdDescription)
+    private let paperKeyCell = SecurityCenterCell(title: S.SecurityCenter.Cells.paperKeyTitle, descriptionText: S.SecurityCenter.Cells.paperKeyDescription)
+    private let separator = UIView(color: .secondaryShadow)
 }
 
 extension SecurityCenterViewController : UIScrollViewDelegate {
