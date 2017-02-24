2ba7b41603455961f94101530454f5ce490cb1aa
Adrian Corscadden <me@acorscadden.com>
2017-02-23 23:56:04 -0800

    Add recover wallet intro screen

--- breadwallet/src/ViewControllers/RecoverWalletIntroViewController.swift
+++ breadwallet/src/ViewControllers/RecoverWalletIntroViewController.swift
@@ -18,18 +18,52 @@ class RecoverWalletIntroViewController : UIViewController {
 
     //MARK: - Private
     private let didTapNext: () -> Void
-    private let header = UIView(color: .purple)
-    private let nextButton = ShadowButton(title: "Next", type: .primary)
+    private let header = RadialGradientView(backgroundColor: .purple)
+    private let nextButton = ShadowButton(title: S.RecoverWallet.next, type: .primary)
+    private let label = UILabel(font: .customBody(size: 16.0))
+    private let illustration = UIImageView(image: #imageLiteral(resourceName: "RecoverWalletIllustration"))
 
     override func viewDidLoad() {
-        view.backgroundColor = .white
+        addSubviews()
+        addConstraints()
+        setData()
+    }
+
+    private func addSubviews() {
+        view.addSubview(header)
+        header.addSubview(illustration)
         view.addSubview(nextButton)
+        view.addSubview(label)
+    }
+
+    private func addConstraints() {
+        header.constrainTopCorners(sidePadding: 0.0, topPadding: 0.0)
+        header.constrain([header.heightAnchor.constraint(equalToConstant: C.Sizes.largeHeaderHeight)])
+        illustration.constrain([
+            illustration.centerXAnchor.constraint(equalTo: header.centerXAnchor),
+            illustration.centerYAnchor.constraint(equalTo: header.centerYAnchor, constant: C.padding[2]) ])
+        label.constrain([
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: C.padding[2]),
+            label.topAnchor.constraint(equalTo: header.bottomAnchor, constant: C.padding[2]),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -C.padding[2]) ])
         nextButton.constrain([
             nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: C.padding[2]),
             nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -C.padding[3]),
             nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -C.padding[2]),
             nextButton.heightAnchor.constraint(equalToConstant: C.Sizes.buttonHeight) ])
+    }
+
+    private func setData() {
+        view.backgroundColor = .white
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.text = S.RecoverWallet.intro
         nextButton.tap = didTapNext
+        title = "Recover Wallet"
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
     required init?(coder aDecoder: NSCoder) {
