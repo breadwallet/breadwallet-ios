62ae007a3bedb89c6a5a41a157ca67ef6fd76ce2
Adrian Corscadden <me@acorscadden.com>
2017-03-02 17:58:07 -0800

    Update background fetch actions

---
+++ breadwallet/src/Extensions/Async.swift
@@ -0,0 +1,24 @@
+//
+//  Async.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-03-02.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import Foundation
+
+enum Async {
+    static func parallel(callbacks: [(@escaping () -> Void) -> Void], completion: @escaping () -> Void) {
+        let dispatchGroup = DispatchGroup()
+        callbacks.forEach { cb in
+            dispatchGroup.enter()
+            cb({
+                dispatchGroup.leave()
+            })
+        }
+        dispatchGroup.notify(queue: .main) { 
+            completion()
+        }
+    }
+}
