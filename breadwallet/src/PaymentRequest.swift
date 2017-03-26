93b03c73e49b153de3087e1102902e3ccb27676a
Adrian Corscadden <me@acorscadden.com>
2017-03-26 16:19:32 -0700

    Add amount to payment request

--- breadwallet/src/PaymentRequest.swift
+++ breadwallet/src/PaymentRequest.swift
@@ -11,18 +11,50 @@ import Foundation
 struct PaymentRequest {
 
     init?(string: String) {
-        if let url = NSURL(string: string) {
-            if url.scheme == "bitcoin" {
-                if let host = url.resourceSpecifier {
+        if var url = NSURL(string: string) {
+
+            if let scheme = url.scheme, let resourceSpecifier = url.resourceSpecifier, url.host == nil {
+                url = NSURL(string: "\(scheme)://\(resourceSpecifier)")!
+
+                if url.scheme == "bitcoin", let host = url.host {
                     toAddress = host
-                } else {
-                    return nil
+
+                    url.query?.components(separatedBy: "&").forEach {
+                        let pair = $0.components(separatedBy: "=")
+                        if pair.count == 2 {
+                            let key = pair[0]
+                            if let value = pair[1].replacingOccurrences(of: "+", with: " ").replacingPercentEscapes(using: .utf8) {
+                                if key == "amount" {
+                                    amount = amount(forValue: value)
+                                }
+                            }
+                        }
+                    }
+
+
+                    return
                 }
-            } else {
-                return nil
+
             }
-        } else if string.utf8.count > 0 {
+
+
+        }
+
+        if string.utf8.count > 0 {
             toAddress = string
+            return
+        }
+
+        return nil
+    }
+
+    private func amount(forValue: String) -> UInt64? {
+        var decimal: Decimal = 0.0
+        var amount: Decimal = 0.0
+
+        if Scanner(string: forValue).scanDecimal(&decimal) {
+            NSDecimalMultiplyByPowerOf10(&amount, &decimal, 8, .up)
+            return NSDecimalNumber(decimal: amount).uint64Value
         } else {
             return nil
         }
---
+++ breadwalletTests/PaymentRequestTests.swift
@@ -0,0 +1,28 @@
+//
+//  PaymentRequestTests.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-03-26.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import XCTest
+@testable import breadwallet
+
+class PaymentRequestTests : XCTestCase {
+
+    func testBasicExample() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu"
+        let request = PaymentRequest(string: uri)
+        XCTAssertNotNil(request)
+        XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
+    }
+
+    func testAmountInUri() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2"
+        let request = PaymentRequest(string: uri)
+        XCTAssertNotNil(request)
+        XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
+        XCTAssertTrue(request?.amount == 120000000)
+    }
+}
