1561f008d5da0f15395f46fa814a21fb1ddd8229
Adrian Corscadden <me@acorscadden.com>
2017-03-27 16:16:08 -0700

    Add Touch ID settings view

---
+++ breadwallet/src/ViewControllers/TouchIdSettingsViewController.swift
@@ -0,0 +1,74 @@
+//
+//  TouchIdSettingsViewController.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-03-27.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class TouchIdSettingsViewController : UIViewController {
+
+    private let header = RadialGradientView(backgroundColor: .darkPurple)
+    private let illustration = UIImageView(image: #imageLiteral(resourceName: "TouchId-Large"))
+    private let label = UILabel.wrapping(font: .customBody(size: 16.0), color: .darkText)
+    private let switchLabel = UILabel(font: .customBold(size: 14.0), color: .darkText)
+    private let toggle = UISwitch()
+    private let separator = UIView(color: .secondaryShadow)
+    private let textView = UITextView()
+
+    override func viewDidLoad() {
+        addSubviews()
+        addConstraints()
+        setData()
+    }
+
+    private func addSubviews() {
+        view.addSubview(header)
+        header.addSubview(illustration)
+        view.addSubview(label)
+        view.addSubview(switchLabel)
+        view.addSubview(toggle)
+        view.addSubview(separator)
+        view.addSubview(textView)
+    }
+
+    private func addConstraints() {
+        header.constrainTopCorners(sidePadding: 0.0, topPadding: 0.0)
+        header.constrain([header.heightAnchor.constraint(equalToConstant: C.Sizes.largeHeaderHeight)])
+        illustration.constrain([
+            illustration.centerXAnchor.constraint(equalTo: header.centerXAnchor),
+            illustration.centerYAnchor.constraint(equalTo: header.centerYAnchor, constant: C.padding[2]) ])
+        label.constrain([
+            label.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: C.padding[2]),
+            label.topAnchor.constraint(equalTo: header.bottomAnchor, constant: C.padding[2]),
+            label.trailingAnchor.constraint(equalTo: header.trailingAnchor, constant: -C.padding[2]) ])
+        switchLabel.constrain([
+            switchLabel.leadingAnchor.constraint(equalTo: label.leadingAnchor),
+            switchLabel.topAnchor.constraint(equalTo: label.bottomAnchor, constant: C.padding[2]) ])
+        toggle.constrain([
+            toggle.centerYAnchor.constraint(equalTo: switchLabel.centerYAnchor),
+            toggle.trailingAnchor.constraint(equalTo: label.trailingAnchor) ])
+        separator.constrain([
+            separator.leadingAnchor.constraint(equalTo: switchLabel.leadingAnchor),
+            separator.topAnchor.constraint(equalTo: toggle.bottomAnchor, constant: C.padding[1]),
+            separator.trailingAnchor.constraint(equalTo: toggle.trailingAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 1.0) ])
+        textView.constrain([
+            textView.leadingAnchor.constraint(equalTo: separator.leadingAnchor),
+            textView.topAnchor.constraint(equalTo: separator.bottomAnchor, constant: C.padding[2]),
+            textView.trailingAnchor.constraint(equalTo: separator.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor )])
+    }
+
+    private func setData() {
+        view.backgroundColor = .white
+        title = S.TouchIdSettings.title
+        label.text = S.TouchIdSettings.label
+        switchLabel.text = S.TouchIdSettings.switchLabel
+        textView.text = S.TouchIdSettings.spendingLimitLabel
+        textView.isSelectable = false
+    }
+
+}
