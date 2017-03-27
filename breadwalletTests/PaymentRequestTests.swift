df1c298ffcc15e8114dc01841958d2c857b4f6b1
Adrian Corscadden <me@acorscadden.com>
2017-03-26 17:40:32 -0700

    Add label and message to payment request

--- breadwalletTests/PaymentRequestTests.swift
+++ breadwalletTests/PaymentRequestTests.swift
@@ -11,6 +11,10 @@ import XCTest
 
 class PaymentRequestTests : XCTestCase {
 
+    func testEmptyString() {
+        XCTAssertNil(PaymentRequest(string: ""))
+    }
+
     func testBasicExample() {
         let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu"
         let request = PaymentRequest(string: uri)
@@ -24,5 +28,20 @@ class PaymentRequestTests : XCTestCase {
         XCTAssertNotNil(request)
         XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
         XCTAssertTrue(request?.amount == 120000000)
+    }
+
+    func testRequestMetaData() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2&message=Payment&label=Satoshi"
+        let request = PaymentRequest(string: uri)
+        XCTAssertTrue(request?.toAddress == "12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu")
+        XCTAssertTrue(request?.amount == 120000000)
+        XCTAssertTrue(request?.message == "Payment")
+        XCTAssertTrue(request?.label == "Satoshi")
+    }
+
+    func testExtraEqualSign() {
+        let uri = "bitcoin:12A1MyfXbW6RhdRAZEqofac5jCQQjwEPBu?amount=1.2&message=Payment=true&label=Satoshi"
+        let request = PaymentRequest(string: uri)
+        XCTAssertTrue(request?.message == "Payment=true")
     }
 }
