a500a83026696c31ec10429db542a6c6a7ca3131
Samuel Sutch <sam@sutch.net>
2017-04-01 12:22:54 -0700

    plat /_webviewinfo socket and didload timeout

--- breadwallet/src/Platform/BRWebViewController.swift
+++ breadwallet/src/Platform/BRWebViewController.swift
@@ -29,7 +29,7 @@ import WebKit
 
 
 @available(iOS 8.0, *)
-@objc open class BRWebViewController : UIViewController, WKNavigationDelegate {
+@objc open class BRWebViewController : UIViewController, WKNavigationDelegate, BRWebSocketClient {
     var wkProcessPool: WKProcessPool
     var webView: WKWebView?
     var bundleName: String
@@ -37,6 +37,24 @@ import WebKit
     var debugEndpoint: String?
     var mountPoint: String
     var walletManager: WalletManager
+    // didLoad should be set to true within didLoadTimeout otherwise a view will be shown which
+    // indicates some error. this is to prevent the white-screen-of-death where there is some
+    // javascript exception (or other error) that prevents the content from loading
+    var didLoad = false
+    var didAppear = false
+    var didLoadTimeout = 2500
+    // we are also a socket server which sends didview/didload events to the listening client(s)
+    var sockets = [String: BRWebSocket]()
+    // this is the data that occasionally gets sent to the above connected sockets
+    var webViewInfo: [String: Any] {
+        return [
+            "visible": didAppear,
+            "loaded": didLoad,
+        ]
+    }
+    var indexUrl: URL {
+        return URL(string: "http://127.0.0.1:\(server.port)\(mountPoint)")!
+    }
     
     init(bundleName name: String, mountPoint mp: String = "/", walletManager wm: WalletManager) {
         wkProcessPool = WKProcessPool()
@@ -55,6 +73,7 @@ import WebKit
     }
     
     override open func loadView() {
+        didLoad = false
         let config = WKWebViewConfiguration()
         config.processPool = wkProcessPool
         config.allowsInlineMediaPlayback = false
@@ -62,7 +81,6 @@ import WebKit
         config.requiresUserActionForMediaPlayback = true
         config.allowsPictureInPictureMediaPlayback = false
 
-        let indexUrl = URL(string: "http://127.0.0.1:\(server.port)\(mountPoint)")!
         let request = URLRequest(url: indexUrl)
         
         view = UIView(frame: CGRect.zero)
@@ -74,10 +92,57 @@ import WebKit
         _ = webView?.load(request)
         webView?.autoresizingMask = [UIViewAutoresizing.flexibleHeight, UIViewAutoresizing.flexibleWidth]
         view.addSubview(webView!)
+        
+        let center = NotificationCenter.default
+        center.addObserver(forName: .UIApplicationDidBecomeActive, object: nil, queue: .main) { (_) in
+            self.didAppear = true
+            self.sendToAllSockets(data: self.webViewInfo)
+        }
+        center.addObserver(forName: .UIApplicationWillResignActive, object: nil, queue: .main) { (_) in
+            self.didAppear = false
+            self.sendToAllSockets(data: self.webViewInfo)
+        }
     }
     
     override open func viewWillAppear(_ animated: Bool) {
         edgesForExtendedLayout = .all
+        self.beginDidLoadCountdown()
+    }
+    
+    override open func viewDidAppear(_ animated: Bool) {
+        didAppear = true
+        sendToAllSockets(data: webViewInfo)
+    }
+    
+    override open func viewDidDisappear(_ animated: Bool) {
+        didAppear = false
+        sendToAllSockets(data: webViewInfo)
+    }
+    
+    // this should be called when the webview is expected to load content. if the content has not signaled
+    // that is has loaded by didLoadTimeout then an alert will be shown allowing the user to back out
+    // of the faulty webview
+    fileprivate func beginDidLoadCountdown() {
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .milliseconds(didLoadTimeout)) {
+            if self.didAppear && !self.didLoad {
+                let alert = UIAlertController.init(
+                    title: NSLocalizedString("Error", comment: ""),
+                    message: NSLocalizedString("There was an error loading the content. Please try again", comment: ""),
+                    preferredStyle: .alert
+                )
+                let action = UIAlertAction(title: NSLocalizedString("Dismiss", comment: ""), style: .default) { _ in
+                    self.closeNow()
+                }
+                alert.addAction(action)
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    // signal to the presenter that the webview content successfully loaded
+    fileprivate func webviewDidLoad() {
+        didLoad = true
+        sendToAllSockets(data: webViewInfo)
     }
     
     fileprivate func closeNow() {
@@ -151,6 +216,17 @@ import WebKit
             return BRHTTPResponse(request: request, code: 204)
         }
         
+        // GET /_didload signals to the presenter that the content successfully loaded
+        router.get("/_didload") { (request, _) -> BRHTTPResponse in
+            DispatchQueue.main.async {
+                self.webviewDidLoad()
+            }
+            return BRHTTPResponse(request: request, code: 204)
+        }
+        
+        // socket /_webviewinfo will send info about the webview state to client
+        router.websocket("/_webviewinfo", client: self)
+        
         router.printDebug()
     }
     
@@ -159,7 +235,8 @@ import WebKit
     }
     
     open func refresh() {
-        _ = webView?.reload()
+        let request = URLRequest(url: indexUrl)
+        _ = webView?.load(request)
     }
     
     // MARK: - navigation delegate
@@ -173,5 +250,44 @@ import WebKit
         }
         print("[BRWebViewController disallowing navigation: \(navigationAction)")
         decisionHandler(.cancel)
+    }
+    
+    // MARK: - socket delegate
+    func sendTo(socket: BRWebSocket, data: [String: Any]) {
+        do {
+            let j = try JSONSerialization.data(withJSONObject: data, options: [])
+            if let s = String(data: j, encoding: .utf8) {
+                socket.request.queue.async {
+                    socket.send(s)
+                }
+            }
+        } catch let e {
+            print("LOCATION SOCKET FAILED ENCODE JSON: \(e)")
+        }
+    }
+    
+    func sendToAllSockets(data: [String: Any]) {
+        for (_, s) in sockets {
+            sendTo(socket: s, data: data)
+        }
+    }
+    
+    public func socketDidConnect(_ socket: BRWebSocket) {
+        print("WEBVIEW SOCKET CONNECT \(socket.id)")
+        sockets[socket.id] = socket
+        sendTo(socket: socket, data: webViewInfo)
+    }
+    
+    public func socketDidDisconnect(_ socket: BRWebSocket) {
+        print("WEBVIEW SOCKET DISCONNECT \(socket.id)")
+        sockets.removeValue(forKey: socket.id)
+    }
+    
+    public func socket(_ socket: BRWebSocket, didReceiveText text: String) {
+        print("WEBVIEW SOCKET RECV TEXT \(text)")
+    }
+    
+    public func socket(_ socket: BRWebSocket, didReceiveData data: Data) {
+        print("WEBVIEW SOCKET RECV TEXT \(data.hexString)")
     }
 }
