28b534e6e69e83ca6ea3abc16a7466bdeaadf968
Samuel Sutch <sam@sutch.net>
2017-04-01 14:19:22 -0700

    refactor kv store out of api client file and make it nicer

---
+++ breadwallet/AssociatedObject.swift
@@ -0,0 +1,33 @@
+//
+//  AssociatedObject.swift
+//  breadwallet
+//
+//  Created by Samuel Sutch on 4/1/17.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import Foundation
+
+func associatedObject<T: AnyObject>(_ base: AnyObject, key: UnsafePointer<UInt8>, initialiser: () -> T) -> T {
+    if let associated = objc_getAssociatedObject(base, key) as? T {
+        return associated
+    }
+    let associated = initialiser()
+    objc_setAssociatedObject(base, key, associated, .OBJC_ASSOCIATION_RETAIN)
+    return associated
+}
+
+func lazyAssociatedObject<T: AnyObject>(_ base: AnyObject, key: UnsafePointer<UInt8>, initialiser: () -> T?) -> T? {
+    if let associated = objc_getAssociatedObject(base, key) as? T {
+        return associated
+    }
+    if let associated = initialiser() {
+        objc_setAssociatedObject(base, key, associated, .OBJC_ASSOCIATION_RETAIN)
+        return associated
+    }
+    return nil
+}
+
+func associateObject<ValueType: AnyObject>(_ base: AnyObject, key: UnsafePointer<UInt8>, value: ValueType) {
+    objc_setAssociatedObject(base, key, value, .OBJC_ASSOCIATION_RETAIN)
+}
