f4c00f6d88b0ed9d5ba61916d08df7d8e83515f1
Samuel Sutch <sam@sutch.net>
2017-04-01 12:08:15 -0700

    plat camera updates: resize image and return fake image in test mode

--- breadwallet/src/Platform/Extensions.swift
+++ breadwallet/src/Platform/Extensions.swift
@@ -26,6 +26,7 @@
 import Foundation
 import BRCore
 import libbz2
+import UIKit
 
 
 public extension String {
@@ -411,5 +412,63 @@ public extension BRKey {
         var data = Data(count: len)
         BRKeyPubKey(&k, data.withUnsafeMutableBytes({ (d: UnsafeMutablePointer<UInt8>) -> UnsafeMutablePointer<UInt8> in d }), len)
         return data
+    }
+}
+
+extension UIImage {
+    
+    /// Represents a scaling mode
+    enum ScalingMode {
+        case aspectFill
+        case aspectFit
+        
+        /// Calculates the aspect ratio between two sizes
+        ///
+        /// - parameters:
+        ///     - size:      the first size used to calculate the ratio
+        ///     - otherSize: the second size used to calculate the ratio
+        ///
+        /// - return: the aspect ratio between the two sizes
+        func aspectRatio(between size: CGSize, and otherSize: CGSize) -> CGFloat {
+            let aspectWidth  = size.width/otherSize.width
+            let aspectHeight = size.height/otherSize.height
+            
+            switch self {
+            case .aspectFill:
+                return max(aspectWidth, aspectHeight)
+            case .aspectFit:
+                return min(aspectWidth, aspectHeight)
+            }
+        }
+    }
+    
+    /// Scales an image to fit within a bounds with a size governed by the passed size. Also keeps the aspect ratio.
+    ///
+    /// - parameters:
+    ///     - newSize:     the size of the bounds the image must fit within.
+    ///     - scalingMode: the desired scaling mode
+    ///
+    /// - returns: a new scaled image.
+    func scaled(to newSize: CGSize, scalingMode: UIImage.ScalingMode = .aspectFill) -> UIImage {
+        
+        let aspectRatio = scalingMode.aspectRatio(between: newSize, and: size)
+        
+        /* Build the rectangle representing the area to be drawn */
+        var scaledImageRect = CGRect.zero
+        
+        scaledImageRect.size.width  = size.width * aspectRatio
+        scaledImageRect.size.height = size.height * aspectRatio
+        scaledImageRect.origin.x    = (newSize.width - size.width * aspectRatio) / 2.0
+        scaledImageRect.origin.y    = (newSize.height - size.height * aspectRatio) / 2.0
+        
+        /* Draw and retrieve the scaled image */
+        UIGraphicsBeginImageContext(newSize)
+        
+        draw(in: scaledImageRect)
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return scaledImage!
     }
 }
