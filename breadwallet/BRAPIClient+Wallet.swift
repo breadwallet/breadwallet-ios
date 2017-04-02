0f62b9dbeca5bcfbb6bc0ba2d71a7ab66dfaeeb0
Samuel Sutch <sam@sutch.net>
2017-04-02 11:41:53 -0700

    platform: more cleanup/refactoring; use more extensions

---
+++ breadwallet/BRAPIClient+Wallet.swift
@@ -0,0 +1,77 @@
+//
+//  BRAPIClient+Wallet.swift
+//  breadwallet
+//
+//  Created by Samuel Sutch on 4/2/17.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import Foundation
+
+extension BRAPIClient {
+    // Fetches the /v1/fee-per-kb endpoint
+    func feePerKb(_ handler: @escaping (_ feePerKb: uint_fast64_t, _ error: String?) -> Void) {
+        let req = URLRequest(url: url("/fee-per-kb"))
+        let task = self.dataTaskWithRequest(req) { (data, response, err) -> Void in
+            var feePerKb: uint_fast64_t = 0
+            var errStr: String? = nil
+            if err == nil {
+                do {
+                    let parsedObject: Any? = try JSONSerialization.jsonObject(
+                        with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
+                    if let top = parsedObject as? NSDictionary, let n = top["fee_per_kb"] as? NSNumber {
+                        feePerKb = n.uint64Value
+                    }
+                } catch (let e) {
+                    self.log("fee-per-kb: error parsing json \(e)")
+                }
+                if feePerKb == 0 {
+                    errStr = "invalid json"
+                }
+            } else {
+                self.log("fee-per-kb network error: \(String(describing: err))")
+                errStr = "bad network connection"
+            }
+            handler(feePerKb, errStr)
+        }
+        task.resume()
+    }
+    
+    func exchangeRates(_ handler: @escaping (_ rates: [Rate], _ error: String?) -> Void) {
+        let request = URLRequest(url: url("/rates"))
+        let task = dataTaskWithRequest(request) { (data, response, error) in
+            guard error == nil else { return handler([], error!.localizedDescription) }
+            guard let data = data else { return handler([], "/rates returned no data") }
+            guard let parsedData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else { return handler([], "/rates bad data format") }
+            guard let dict = parsedData as? [String: Any] else { return handler([], "/rates didn't return a dictionary") }
+            guard let array = dict["body"] as? [Any] else { return handler([], "/rates didn't return an array for body key") }
+            handler(array.flatMap { Rate(data: $0) }, nil)
+        }
+        task.resume()
+    }
+    
+    // MARK: push notifications
+    
+    func savePushNotificationToken(_ token: Data, pushNotificationType: String = "d") {
+        var req = URLRequest(url: url("/me/push-devices"))
+        req.httpMethod = "POST"
+        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        req.setValue("application/json", forHTTPHeaderField: "Accept")
+        let reqJson = [
+            "token": token.hexString,
+            "service": "apns",
+            "data": ["e": pushNotificationType]
+            ] as [String : Any]
+        do {
+            let dat = try JSONSerialization.data(withJSONObject: reqJson, options: .prettyPrinted)
+            req.httpBody = dat
+        } catch (let e) {
+            log("JSON Serialization error \(e)")
+            return
+        }
+        dataTaskWithRequest(req as URLRequest, authenticated: true, retryCount: 0) { (dat, resp, er) in
+            let dat2 = String(data: dat ?? Data(), encoding: .utf8)
+            self.log("save push token resp: \(String(describing: resp)) data: \(String(describing: dat2))")
+            }.resume()
+    }
+}
--- breadwallet/src/Platform/BRAPIClient.swift
+++ breadwallet/src/Platform/BRAPIClient.swift
@@ -60,95 +60,11 @@ public protocol BRAPIAdaptor {
     ) -> URLSessionDataTask
 }
 
-extension String {
-    static var urlQuoteCharacterSet: CharacterSet {
-        let cset = (NSMutableCharacterSet.urlQueryAllowed as NSCharacterSet).mutableCopy() as! NSMutableCharacterSet
-        cset.removeCharacters(in: "?=&")
-        return cset as CharacterSet
-    }
-    
-    var urlEscapedString: String {
-        return self.addingPercentEncoding(
-            withAllowedCharacters: String.urlQuoteCharacterSet)!
-    }
-}
-
-func getHeaderValue(_ k: String, d: [String: String]?) -> String? {
-    guard let d = d else {
-        return nil
-    }
-    if let v = d[k] { // short path: attempt to get the header directly
-        return v
-    }
-    let lkKey = k.lowercased() // long path: compare lowercase keys
-    for (lk, lv) in d {
-        if lk.lowercased() == lkKey {
-            return lv
-        }
-    }
-    return nil
-}
-
-func getDeviceId() -> String {
-    let ud = UserDefaults.standard
-    if let s = ud.string(forKey: "BR_DEVICE_ID") {
-        return s
-    }
-    let s = CFUUIDCreateString(nil, CFUUIDCreate(nil)) as String
-    ud.setValue(s, forKey: "BR_DEVICE_ID")
-    print("new device id \(s)")
-    return s
-}
-
-func isBreadChallenge(_ r: HTTPURLResponse) -> Bool {
-    if let headers = r.allHeaderFields as? [String: String],
-       let challenge = getHeaderValue("www-authenticate", d: headers) {
-        if challenge.lowercased().hasPrefix("bread") {
-            return true
-        }
-    }
-    return false
-}
-
-func buildURLResourceString(_ url: URL?) -> String {
-    var urlStr = ""
-    if let url = url {
-        urlStr = "\(url.path)"
-        if let query = url.query {
-            if query.lengthOfBytes(using: String.Encoding.utf8) > 0 {
-                urlStr = "\(urlStr)?\(query)"
-            }
-        }
-    }
-    return urlStr
-}
-
-func buildRequestSigningString(_ r: URLRequest) -> String {
-    var parts = [
-        r.httpMethod ?? "",
-        "",
-        getHeaderValue("content-type", d: r.allHTTPHeaderFields) ?? "",
-        getHeaderValue("date", d: r.allHTTPHeaderFields) ?? "",
-        buildURLResourceString(r.url)
-    ]
-    if let meth = r.httpMethod {
-        switch meth {
-        case "POST", "PUT", "PATCH":
-            if let d = r.httpBody , d.count > 0 {
-                parts[1] = d.sha256.base58
-            }
-        default: break
-        }
-    }
-    return parts.joined(separator: "\n")
-}
-
-
 open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, BRAPIAdaptor {
-    var authenticator: WalletAuthenticator
+    private var authenticator: WalletAuthenticator
     
     // whether or not to emit log messages from this instance of the client
-    var logEnabled = true
+    private var logEnabled = true
     
     // proto is the transport protocol to use for talking to the API (either http or https)
     var proto = "https"
@@ -159,19 +75,19 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
     // isFetchingAuth is set to true when a request is currently trying to renew authentication (the token)
     // it is useful because fetching auth is not idempotent and not reentrant, so at most one auth attempt
     // can take place at any one time
-    fileprivate var isFetchingAuth = false
+    private var isFetchingAuth = false
     
     // used when requests are waiting for authentication to be fetched
-    fileprivate var authFetchGroup = DispatchGroup()
+    private var authFetchGroup = DispatchGroup()
 
     // the NSURLSession on which all NSURLSessionTasks are executed
-    lazy fileprivate var session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: self.queue)
+    lazy private var session: URLSession = URLSession(configuration: .default, delegate: self, delegateQueue: self.queue)
 
     // the queue on which the NSURLSession operates
-    fileprivate var queue = OperationQueue()
+    private var queue = OperationQueue()
     
     // convenience getter for the API endpoint
-    var baseUrl: String {
+    private var baseUrl: String {
         return "\(proto)://\(host)"
     }
     
@@ -188,7 +104,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
     }
     
     var deviceId: String {
-        return getDeviceId()
+        return UserDefaults.standard.deviceID
     }
     
     var authKey: BRKey? {
@@ -221,9 +137,9 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
         }
     }
     
-    func signRequest(_ request: URLRequest) -> URLRequest {
+    private func signRequest(_ request: URLRequest) -> URLRequest {
         var mutableRequest = request
-        let dateHeader = getHeaderValue("date", d: mutableRequest.allHTTPHeaderFields)
+        let dateHeader = mutableRequest.allHTTPHeaderFields?.get(lowercasedKey: "date")
         if dateHeader == nil {
             // add Date header if necessary
             mutableRequest.setValue(Date().RFC1123String(), forHTTPHeaderField: "Date")
@@ -231,7 +147,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
         if let tokenData = authenticator.userAccount,
             let token = tokenData["token"] as? String,
             let authKey = authKey,
-            let signingData = buildRequestSigningString(mutableRequest).data(using: .utf8) {
+            let signingData = mutableRequest.signingString.data(using: .utf8) {
             let sig = signingData.sha256_2.compactSign(key: authKey)
             let hval = "bread \(token):\(sig.base58)"
             mutableRequest.setValue(hval, forHTTPHeaderField: "Authorization")
@@ -239,7 +155,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
         return mutableRequest
     }
     
-    func decorateRequest(_ request: URLRequest) -> URLRequest {
+    private func decorateRequest(_ request: URLRequest) -> URLRequest {
         var actualRequest = request
         #if Testnet
             let testnet = true
@@ -257,7 +173,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
         return actualRequest
     }
     
-    open func dataTaskWithRequest(_ request: URLRequest, authenticated: Bool = false,
+    public func dataTaskWithRequest(_ request: URLRequest, authenticated: Bool = false,
                              retryCount: Int = 0, handler: @escaping URLSessionTaskHandler) -> URLSessionDataTask {
         let start = Date()
         var logLine = ""
@@ -284,7 +200,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
                     
                     self.log("\(logLine) -> status=\(httpResp.statusCode) duration=\(dur)ms errStr=\(errStr)")
                     
-                    if authenticated && isBreadChallenge(httpResp) {
+                    if authenticated && httpResp.isBreadChallenge {
                         self.log("\(logLine) got authentication challenge from API - will attempt to get token")
                         self.getToken { err in
                             if err != nil && retryCount < 1 { // retry once
@@ -320,7 +236,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
     }
     
     // retrieve a token and save it in the keychain data for this account
-    func getToken(_ handler: @escaping (NSError?) -> Void) {
+    private func getToken(_ handler: @escaping (NSError?) -> Void) {
         if isFetchingAuth {
             log("already fetching auth, waiting...")
             authFetchGroup.notify(queue: DispatchQueue.main) {
@@ -342,7 +258,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
         req.setValue("application/json", forHTTPHeaderField: "Accept")
         let reqJson = [
             "pubKey": authPubKey.base58,
-            "deviceID": getDeviceId()
+            "deviceID": deviceId
         ]
         do {
             let dat = try JSONSerialization.data(withJSONObject: reqJson, options: [])
@@ -427,119 +343,65 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
         }
         completionHandler(nil)
     }
-    
-    // MARK: API Functions
-    
-    // Fetches the /v1/fee-per-kb endpoint
-    open func feePerKb(_ handler: @escaping (_ feePerKb: uint_fast64_t, _ error: String?) -> Void) {
-        let req = URLRequest(url: url("/fee-per-kb"))
-        let task = self.dataTaskWithRequest(req) { (data, response, err) -> Void in
-            var feePerKb: uint_fast64_t = 0
-            var errStr: String? = nil
-            if err == nil {
-                do {
-                    let parsedObject: Any? = try JSONSerialization.jsonObject(
-                        with: data!, options: JSONSerialization.ReadingOptions.allowFragments)
-                    if let top = parsedObject as? NSDictionary, let n = top["fee_per_kb"] as? NSNumber {
-                        feePerKb = n.uint64Value
-                    }
-                } catch (let e) {
-                    self.log("fee-per-kb: error parsing json \(e)")
-                }
-                if feePerKb == 0 {
-                    errStr = "invalid json"
-                }
-            } else {
-                self.log("fee-per-kb network error: \(String(describing: err))")
-                errStr = "bad network connection"
-            }
-            handler(feePerKb, errStr)
-        }
-        task.resume()
-    }
+}
 
-    func exchangeRates(_ handler: @escaping (_ rates: [Rate], _ error: String?) -> Void) {
-        let request = URLRequest(url: url("/rates"))
-        let task = dataTaskWithRequest(request) { (data, response, error) in
-            guard error == nil else { return handler([], error!.localizedDescription) }
-            guard let data = data else { return handler([], "/rates returned no data") }
-            guard let parsedData = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else { return handler([], "/rates bad data format") }
-            guard let dict = parsedData as? [String: Any] else { return handler([], "/rates didn't return a dictionary") }
-            guard let array = dict["body"] as? [Any] else { return handler([], "/rates didn't return an array for body key") }
-            handler(array.flatMap { Rate(data: $0) }, nil)
+extension Dictionary where Key == String, Value == String {
+    func get(lowercasedKey k: String) -> String? {
+        let lcKey = k.lowercased()
+        if let v = self[lcKey] {
+            return v
         }
-        task.resume()
-    }
-    
-    // MARK: push notifications
-    
-    open func savePushNotificationToken(_ token: Data, pushNotificationType: String = "d") {
-        var req = URLRequest(url: url("/me/push-devices"))
-        req.httpMethod = "POST"
-        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        req.setValue("application/json", forHTTPHeaderField: "Accept")
-        let reqJson = [
-            "token": token.hexString,
-            "service": "apns",
-            "data": ["e": pushNotificationType]
-        ] as [String : Any]
-        do {
-            let dat = try JSONSerialization.data(withJSONObject: reqJson, options: .prettyPrinted)
-            req.httpBody = dat
-        } catch (let e) {
-            log("JSON Serialization error \(e)")
-            return
-        }
-        dataTaskWithRequest(req as URLRequest, authenticated: true, retryCount: 0) { (dat, resp, er) in
-            let dat2 = String(data: dat ?? Data(), encoding: .utf8)
-            self.log("save push token resp: \(String(describing: resp)) data: \(String(describing: dat2))")
-        }.resume()
-    }
-    
-    // MARK: feature flags API
-    
-    open func defaultsKeyForFeatureFlag(_ name: String) -> String {
-        return "ff:\(name)"
-    }
-    
-    open func updateFeatureFlags() {
-        var authenticated = false
-        var furl = "/anybody/features"
-        // only use authentication if the user has previously used authenticated services
-        if let _ = authenticator.userAccount?["token"] {
-            authenticated = true
-            furl = "/me/features"
-        }
-        let req = URLRequest(url: url(furl))
-        dataTaskWithRequest(req, authenticated: authenticated) { (data, resp, err) in
-            if let resp = resp, let data = data {
-                if resp.statusCode == 200 {
-                    let defaults = UserDefaults.standard
-                    do {
-                        let j = try JSONSerialization.jsonObject(with: data, options: [])
-                        let features = j as! [[String: AnyObject]]
-                        for feat in features {
-                            if let fn = feat["name"], let fname = fn as? String,
-                                let fe = feat["enabled"], let fenabled = fe as? Bool {
-                                self.log("feature \(fname) enabled: \(fenabled)")
-                                defaults.set(fenabled, forKey: self.defaultsKeyForFeatureFlag(fname))
-                            } else {
-                                self.log("malformed feature: \(feat)")
-                            }
-                        }
-                    } catch let e {
-                        self.log("error loading features json: \(e)")
-                    }
-                }
-            } else {
-                self.log("error fetching features: \(String(describing: err))")
+        for (lk, v) in self {
+            if lk.lowercased() == lcKey {
+                return v
             }
-        }.resume()
-    }
-    
-    open func featureEnabled(_ flag: BRFeatureFlags) -> Bool {
-        let defaults = UserDefaults.standard
-        return defaults.bool(forKey: defaultsKeyForFeatureFlag(flag.description))
+        }
+        return nil
     }
 }
 
+fileprivate extension URLRequest {
+    var signingString: String {
+        var parts = [
+            httpMethod ?? "",
+            "",
+            allHTTPHeaderFields?.get(lowercasedKey: "content-type") ?? "",
+            allHTTPHeaderFields?.get(lowercasedKey: "date") ?? "",
+            url?.resourceString ?? ""
+        ]
+        if let meth = httpMethod {
+            switch meth {
+            case "POST", "PUT", "PATCH":
+                if let d = httpBody , d.count > 0 {
+                    parts[1] = d.sha256.base58
+                }
+            default: break
+            }
+        }
+        return parts.joined(separator: "\n")
+    }
+}
+
+fileprivate extension HTTPURLResponse {
+    var isBreadChallenge: Bool {
+        if let headers = allHeaderFields as? [String: String],
+            let challenge = headers.get(lowercasedKey: "www-authenticate") {
+            if challenge.lowercased().hasPrefix("bread") {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+fileprivate extension URL {
+    var resourceString: String {
+        var urlStr = "\(path)"
+        if let query = query {
+            if query.lengthOfBytes(using: String.Encoding.utf8) > 0 {
+                urlStr = "\(urlStr)?\(query)"
+            }
+        }
+        return urlStr
+    }
+}
