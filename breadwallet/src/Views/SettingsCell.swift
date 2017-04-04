4777cddbbd37b47fa9f34c12c254a3d1d0694c5a
Adrian Corscadden <me@acorscadden.com>
2017-04-04 11:34:00 -0700

    Add settings view

---
+++ breadwallet/src/Views/SettingsCell.swift
@@ -0,0 +1,27 @@
+//
+//  SettingsCell.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-04-01.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell : UITableViewCell {
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        let separator = UIView()
+        separator.backgroundColor = .secondaryShadow
+        addSubview(separator)
+        separator.constrain([
+            separator.leadingAnchor.constraint(equalTo: leadingAnchor),
+            separator.bottomAnchor.constraint(equalTo: bottomAnchor),
+            separator.trailingAnchor.constraint(equalTo: trailingAnchor),
+            separator.heightAnchor.constraint(equalToConstant: 1.0) ])
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
