bc9a6d14a25d333706859bc6e0a24dc66c1b03d6
Adrian Corscadden <me@acorscadden.com>
2017-04-16 11:10:52 -0700

    Add custom cursor view to send view

---
+++ breadwallet/src/Views/BlinkingView.swift
@@ -0,0 +1,36 @@
+//
+//  BlinkingView.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-04-16.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+class BlinkingView : UIView {
+
+    init(blinkColor: UIColor) {
+        self.blinkColor = blinkColor
+        super.init(frame: .zero)
+    }
+
+    func startBlinking() {
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(update), userInfo: nil, repeats: true)
+    }
+
+    func stopBlinking() {
+        timer?.invalidate()
+    }
+
+    @objc private func update() {
+        backgroundColor = backgroundColor == .clear ? blinkColor : .clear
+    }
+
+    private let blinkColor: UIColor
+    private var timer: Timer?
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
