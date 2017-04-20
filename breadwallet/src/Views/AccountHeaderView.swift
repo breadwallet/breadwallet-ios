d96e64342782cca17af2ca5e1f740c3b3b8f0d69
Adrian Corscadden <me@acorscadden.com>
2017-04-20 00:32:15 -0700

    Add currency swap animation

--- breadwallet/src/Views/AccountHeaderView.swift
+++ breadwallet/src/Views/AccountHeaderView.swift
@@ -30,12 +30,12 @@ class AccountHeaderView : UIView, GradientDrawable, Subscriber {
     private let manage = UIButton(type: .system)
     private let primaryBalance = UpdatingLabel(formatter: Amount.btcFormat)
     private let secondaryBalance = UpdatingLabel(formatter: Amount.localFormat)
-    private let info = UILabel()
     private let search = UIButton(type: .system)
     private let currencyTapView = UIView()
     private let store: Store
-    //TODO - add equal sign logo
-
+    private let equals = UILabel(font: .customBody(size: 13.0), color: .darkText)
+    private var regularConstraints: [NSLayoutConstraint] = []
+    private var swappedConstraints: [NSLayoutConstraint] = []
     private var exchangeRate: Rate? {
         didSet { setBalances() }
     }
@@ -66,15 +66,14 @@ class AccountHeaderView : UIView, GradientDrawable, Subscriber {
         secondaryBalance.textColor = .darkText
         secondaryBalance.font = UIFont.customBody(size: 13.0)
 
-        info.textColor = .darkText
-        info.font = UIFont.customBody(size: 13.0)
-
         search.setImage(#imageLiteral(resourceName: "SearchIcon"), for: .normal)
         search.tintColor = .white
 
         if isTestnet {
             name.textColor = .red
         }
+
+        equals.text = S.AccountHeader.equals
     }
 
     private func addSubviews() {
@@ -82,9 +81,9 @@ class AccountHeaderView : UIView, GradientDrawable, Subscriber {
         addSubview(manage)
         addSubview(primaryBalance)
         addSubview(secondaryBalance)
-        addSubview(info)
         addSubview(search)
         addSubview(currencyTapView)
+        addSubview(equals)
     }
 
     private func addConstraints() {
@@ -94,27 +93,44 @@ class AccountHeaderView : UIView, GradientDrawable, Subscriber {
         if let manageTitleLabel = manage.titleLabel {
             manage.constrain([
                 manage.constraint(.trailing, toView: self, constant: -C.padding[2]),
-                manageTitleLabel.firstBaselineAnchor.constraint(equalTo: name.firstBaselineAnchor) ])
+                manageTitleLabel.firstBaselineAnchor.constraint(equalTo: name.firstBaselineAnchor)
+                ])
         }
-        primaryBalance.constrain([
-            primaryBalance.constraint(.leading, toView: self, constant: C.padding[2]),
-            primaryBalance.constraint(toBottom: name, constant: C.padding[2]) ])
         secondaryBalance.constrain([
-            secondaryBalance.constraint(toTrailing: primaryBalance, constant: C.padding[1]/2.0),
             secondaryBalance.constraint(.firstBaseline, toView: primaryBalance, constant: 0.0) ])
-        info.constrain([
-            info.constraint(.leading, toView: self, constant: C.padding[2]),
-            info.constraint(toBottom: secondaryBalance, constant: C.padding[1]/2.0) ])
+
+        equals.translatesAutoresizingMaskIntoConstraints = false
+        primaryBalance.translatesAutoresizingMaskIntoConstraints = false
+
+        regularConstraints = [
+            primaryBalance.firstBaselineAnchor.constraint(equalTo: bottomAnchor, constant: -C.padding[4]),
+            primaryBalance.leadingAnchor.constraint(equalTo: leadingAnchor, constant: C.padding[2]),
+            equals.firstBaselineAnchor.constraint(equalTo: primaryBalance.firstBaselineAnchor),
+            equals.leadingAnchor.constraint(equalTo: primaryBalance.trailingAnchor, constant: C.padding[1]/2.0),
+            secondaryBalance.leadingAnchor.constraint(equalTo: equals.trailingAnchor, constant: C.padding[1]/2.0)
+        ]
+
+        NSLayoutConstraint.activate(regularConstraints)
+
+        swappedConstraints = [
+            secondaryBalance.firstBaselineAnchor.constraint(equalTo: bottomAnchor, constant: -C.padding[4]),
+            secondaryBalance.leadingAnchor.constraint(equalTo: leadingAnchor, constant: C.padding[2]),
+            equals.firstBaselineAnchor.constraint(equalTo: secondaryBalance.firstBaselineAnchor),
+            equals.leadingAnchor.constraint(equalTo: secondaryBalance.trailingAnchor, constant: C.padding[1]/2.0),
+            primaryBalance.leadingAnchor.constraint(equalTo: equals.trailingAnchor, constant: C.padding[1]/2.0)
+        ]
+
         search.constrain([
             search.constraint(.trailing, toView: self, constant: -C.padding[2]),
-            search.constraint(.bottom, toView: primaryBalance, constant: 0.0),
+            search.topAnchor.constraint(equalTo: manage.bottomAnchor, constant: C.padding[1]),
             search.constraint(.width, constant: 24.0),
             search.constraint(.height, constant: 24.0) ])
+
         currencyTapView.constrain([
-            currencyTapView.leadingAnchor.constraint(equalTo: primaryBalance.leadingAnchor, constant: -C.padding[1]),
-            currencyTapView.trailingAnchor.constraint(equalTo: secondaryBalance.trailingAnchor, constant: C.padding[1]),
-            currencyTapView.topAnchor.constraint(equalTo: primaryBalance.topAnchor),
-            currencyTapView.bottomAnchor.constraint(equalTo: primaryBalance.bottomAnchor) ])
+            currencyTapView.leadingAnchor.constraint(equalTo: name.leadingAnchor, constant: -C.padding[1]),
+            currencyTapView.trailingAnchor.constraint(equalTo: manage.leadingAnchor, constant: C.padding[1]),
+            currencyTapView.topAnchor.constraint(equalTo: primaryBalance.topAnchor, constant: -C.padding[1]),
+            currencyTapView.bottomAnchor.constraint(equalTo: primaryBalance.bottomAnchor, constant: C.padding[1]) ])
 
         let gr = UITapGestureRecognizer(target: self, action: #selector(currencySwitchTapped))
         currencyTapView.addGestureRecognizer(gr)
@@ -140,11 +156,8 @@ class AccountHeaderView : UIView, GradientDrawable, Subscriber {
         guard let rate = exchangeRate else { return }
         let amount = Amount(amount: balance, rate: rate.rate)
 
-        primaryBalance.formatter = currency == .bitcoin ? Amount.btcFormat : Amount.localFormat
-        secondaryBalance.formatter = currency == .bitcoin ? Amount.localFormat : Amount.btcFormat
-
-        primaryBalance.setValue(currency == .bitcoin ? amount.bitsAmount : amount.localAmount)
-        secondaryBalance.setValue(currency == .bitcoin ? amount.localAmount : amount.bitsAmount)
+        primaryBalance.setValue(amount.bitsAmount)
+        secondaryBalance.setValue(amount.localAmount)
     }
 
     override func draw(_ rect: CGRect) {
@@ -152,6 +165,18 @@ class AccountHeaderView : UIView, GradientDrawable, Subscriber {
     }
 
     @objc private func currencySwitchTapped() {
+        layoutIfNeeded()
+        let willSwap = currency == .bitcoin
+        //TODO - add font animation with CATextLayer
+        self.primaryBalance.textColor = willSwap ? .darkText : .white
+        self.secondaryBalance.textColor = willSwap ? .white : .darkText
+        UIView.spring(C.animationDuration, animations: {
+            NSLayoutConstraint.deactivate(willSwap ? self.regularConstraints : self.swappedConstraints)
+            NSLayoutConstraint.activate(willSwap ? self.swappedConstraints : self.regularConstraints)
+            self.layoutIfNeeded()
+        }) { _ in
+        }
+
         self.store.perform(action: CurrencyChange.toggle())
     }
 
