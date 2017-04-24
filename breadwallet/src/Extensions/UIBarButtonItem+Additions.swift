52fb0b88b2f452784a6b454885b3df8c96c84670
Adrian Corscadden <me@acorscadden.com>
2017-04-24 15:39:09 -0700

    Fix start paper phrase security center button

---
+++ breadwallet/src/Extensions/UIBarButtonItem+Additions.swift
@@ -0,0 +1,17 @@
+//
+//  UIBarButtonItem+Additions.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-04-24.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+extension UIBarButtonItem {
+    static var negativePadding: UIBarButtonItem {
+        let padding = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        padding.width = -16.0
+        return padding
+    }
+}
