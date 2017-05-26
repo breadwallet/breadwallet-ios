d074c8c483e6482f0dcca5b5b2d240bdba7677da
Adrian Corscadden <me@acorscadden.com>
2017-05-26 11:30:02 -0700

    Add urlcontroller skeleton

--- breadwallet/AppDelegate.swift
+++ breadwallet/AppDelegate.swift
@@ -67,5 +67,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         applicationController.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
     }
 
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return applicationController.open(url: url)
+    }
+
 }
 
