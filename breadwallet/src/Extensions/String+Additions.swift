c1731adc7658df72e5a4b6fec3f930aa69d8ee04
Adrian Corscadden <me@acorscadden.com>
2017-05-27 13:51:06 -0700

    Add support for <b> tags in localized strings

--- breadwallet/src/Extensions/String+Additions.swift
+++ breadwallet/src/Extensions/String+Additions.swift
@@ -25,3 +25,34 @@ extension String {
         return applyingTransform(.toUnicodeName, reverse: false) ?? ""
     }
 }
+
+private let startTag = "<b>"
+private let endTag = "</b>"
+
+//Convert string with <b> tags to attributed string
+extension String {
+    var tagsRemoved: String {
+        return replacingOccurrences(of: startTag, with: "").replacingOccurrences(of: endTag, with: "")
+    }
+
+    var attributedStringForTags: NSAttributedString {
+        let output = NSMutableAttributedString()
+        let scanner = Scanner(string: self)
+        let endCount = tagsRemoved.utf8.count
+        var i = 0
+        while output.string.utf8.count < endCount || i < 50 {
+            var regular: NSString?
+            var bold: NSString?
+            scanner.scanUpTo(startTag, into: &regular)
+            scanner.scanUpTo(endTag, into: &bold)
+            if let regular = regular {
+                output.append(NSAttributedString(string: (regular as String).tagsRemoved, attributes: UIFont.regularAttributes))
+            }
+            if let bold = bold {
+                output.append(NSAttributedString(string: (bold as String).tagsRemoved, attributes: UIFont.boldAttributes))
+            }
+            i += 1
+        }
+        return output
+    }
+}
