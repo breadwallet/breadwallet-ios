74d4a04b6a04ceb6c2877c679c3f8aa59b3ca59e
Adrian Corscadden <me@acorscadden.com>
2017-06-04 20:40:59 -0700

    Add better touch target for area for copy address on send view

--- breadwallet/src/Extensions/UIImage+Utils.swift
+++ breadwallet/src/Extensions/UIImage+Utils.swift
@@ -55,4 +55,15 @@ extension UIImage {
         context.draw(cgImage, in: context.boundingBoxOfClipPath)
         return UIGraphicsGetImageFromCurrentImageContext()
     }
+
+    static func imageForColor(_ color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1.0, height: 1.0)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image ?? UIImage()
+    }
 }
