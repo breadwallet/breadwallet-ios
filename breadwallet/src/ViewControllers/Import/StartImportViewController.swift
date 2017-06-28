e3060eaf40b6e4189f90dd9a3ce64e61294c9b92
Adrian Corscadden <me@acorscadden.com>
2017-06-28 15:49:08 -0700

    Update api url

--- breadwallet/src/ViewControllers/Import/StartImportViewController.swift
+++ breadwallet/src/ViewControllers/Import/StartImportViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import BRCore
 
-private let mainURL = "https://api.breadwallet.com/q/addrs/utxo"
+private let mainURL = "https://prod.breadwallet.com/q/addrs/utxo"
 private let fallbackURL = "https://insight.bitpay.com/api/addrs/utxo"
 private let testnetURL = "https://test-insight.bitpay.com/api/addrs/utxo"
 
