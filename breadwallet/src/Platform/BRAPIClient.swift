f6a1aea3ab281320818422bce1a160acb06f5d33
Adrian Corscadden <me@acorscadden.com>
2017-07-04 10:19:55 -0700

    Update api url

--- breadwallet/src/Platform/BRAPIClient.swift
+++ breadwallet/src/Platform/BRAPIClient.swift
@@ -73,7 +73,7 @@ open class BRAPIClient : NSObject, URLSessionDelegate, URLSessionTaskDelegate, B
     #if Testflight || Debug
     var host = "stage.breadwallet.com"
     #else
-    var host = "prod.breadwallet.com"
+    var host = "api.breadwallet.com"
     #endif
     
     // isFetchingAuth is set to true when a request is currently trying to renew authentication (the token)
