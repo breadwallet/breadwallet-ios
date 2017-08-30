73e8cf7ded7339a0973833ab9122fdd96316b8fa
Adrian Corscadden <me@acorscadden.com>
2017-08-30 08:36:31 -0700

    Add new green for tx cell icons

--- breadwallet/src/Views/TransactionCells/TransactionTableViewCell.swift
+++ breadwallet/src/Views/TransactionCells/TransactionTableViewCell.swift
@@ -70,7 +70,7 @@ class TransactionTableViewCell : UITableViewCell, Subscriber {
         let identity: CGAffineTransform = .identity
         if transaction.direction == .received {
             arrow.transform = identity.rotated(by: π/2.0)
-            arrow.tintColor = UIColor(red: 0.4, green: 0.9, blue: 0.4, alpha: 1.0)
+            arrow.tintColor = UIColor(red: 0.0, green: 169.0/255.0, blue: 157.0/255.0, alpha: 1.0)
         } else {
             arrow.transform = identity.rotated(by: 3.0*π/2.0)
             arrow.tintColor = .cameraGuideNegative
