703ed77842bbfefc67f6358ec1f769f6a129c5d1
Adrian Corscadden <me@acorscadden.com>
2017-09-28 14:50:13 -0700

    Fix iPad keyboard when launched in landscape

---
+++ breadwallet/src/Extensions/UIScreen+Additions.swift
@@ -0,0 +1,15 @@
+//
+//  UIScreen+Additions.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-09-28.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import UIKit
+
+extension UIScreen {
+    var safeWidth: CGFloat {
+        return min(bounds.width, bounds.height)
+    }
+}
