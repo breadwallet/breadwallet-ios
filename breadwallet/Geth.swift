21493ab7a5e81348669cadbdaf94fd89d7c63a1a
Aaron Voisine <voisine@gmail.com>
2017-10-25 16:36:47 -0700

    fix GethKeyStore params

--- breadwallet/Geth.swift
+++ breadwallet/Geth.swift
@@ -33,19 +33,21 @@ class GethManager {
 
         //addr = GethAddress(fromBytes: GethHash(fromBytes: Data(bytes: ethPubKey)).getBytes())
         addr = autoreleasepool {
-            let ks = GethKeyStore(NSTemporaryDirectory(), scryptN:1, scryptP:1)
+            let ks = GethKeyStore(NSTemporaryDirectory(), scryptN:2, scryptP:1)
             var data = CFDataCreateMutable(secureAllocator, MemoryLayout<UInt256>.stride) as Data
             data.count = MemoryLayout<UInt256>.stride
             var key = BRKey(privKey: ethPrivKey)
             data.withUnsafeMutableBytes({ $0.pointee = key!.secret })
             key!.clean()
-//            let account = try! ks?.importECDSAKey(data, passphrase: ethPrivKey)
-//            let address = account!.getAddress()
-//            try? ks?.delete(account!, passphrase: ethPrivKey)
-//            return address!
-            return GethNewAddressFromHex("0x53Bb60807caDD27a656fC92Ff4E6733DFCbCb74D", nil)
+            let account = try! ks?.importECDSAKey(data, passphrase: ethPrivKey)
+            let address = account!.getAddress()
+            try? ks?.delete(account!, passphrase: ethPrivKey)
+            return address!
+
+//            return GethNewAddressFromHex("0x53Bb60807caDD27a656fC92Ff4E6733DFCbCb74D", nil)
         }
         
+        print("receive address:\(addr.getHex())")
         print("latest block:\(try! ec.getBlockByNumber(ctx, number: -1).getNumber())")
     }
 
@@ -61,7 +63,7 @@ class GethManager {
     
     func signTx(_ tx: GethTransaction, ethPrivKey: String) -> GethTransaction {
         return autoreleasepool {
-            let ks = GethKeyStore(NSTemporaryDirectory(), scryptN:1, scryptP:1)
+            let ks = GethKeyStore(NSTemporaryDirectory(), scryptN:2, scryptP:1)
             var data = CFDataCreateMutable(secureAllocator, MemoryLayout<UInt256>.stride) as Data
             data.count = MemoryLayout<UInt256>.stride
             var key = BRKey(privKey: ethPrivKey)
