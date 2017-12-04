714759b640a3ed4f8905e039a1c3990941289dfb
Adrian Corscadden <me@acorscadden.com>
2017-12-04 07:08:53 -0800

    Add new total assets style

--- breadwallet/src/ViewControllers/HomeScreenViewController.swift
+++ breadwallet/src/ViewControllers/HomeScreenViewController.swift
@@ -8,88 +8,94 @@
 
 import UIKit
 
-class HomeScreenViewController : UITableViewController, Subscriber {
-
+class HomeScreenViewController : UIViewController, Subscriber {
     init(stores: [Store]) {
         self.stores = stores
+        self.currencyList = AssetListTableView(stores: stores)
         super.init(nibName: nil, bundle: nil)
     }
 
+    private let stores: [Store]
+    private let currencyList: AssetListTableView
+    private let subHeaderView = UIView()
+    private let logo = UIImageView(image:#imageLiteral(resourceName: "LogoGradient"))
+    private let total = UILabel(font: .customMedium(size: 18.0), color: .darkText)
+    private let totalHeader = UILabel(font: .customMedium(size: 14.0))
+
     var didSelectCurrency : ((String) -> Void)?
 
-    private let stores: [Store]
-    private let cellIdentifier = "CellIdentifier"
-    private let crowdSaleCellIdentifier = "CrowdsaleCellIdentifier"
-
-    let currencies: [(String, String)] = {
-        return [("Bitcoin", "btc"), ("Ethereum", "eth")] + tokens.map {
-            return ($0.name, $0.code)
-        }
-    }()
-
     override func viewDidLoad() {
-        tableView.backgroundColor = UIColor(red:0.960784, green:0.968627, blue:0.980392, alpha:1.0)
-        tableView.register(HomeScreenCell.self, forCellReuseIdentifier: cellIdentifier)
-        tableView.register(CrowsaleCell.self, forCellReuseIdentifier: crowdSaleCellIdentifier)
-        tableView.separatorStyle = .none
-        NSLayoutConstraint.activate([
-            view.heightAnchor.constraint(equalToConstant: 200.0) ])
+        view.backgroundColor = .white
+        view.addSubview(subHeaderView)
+        let height: CGFloat = 46.0
+        if #available(iOS 11.0, *) {
+            subHeaderView.constrain([
+                subHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                subHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0.0),
+                subHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                subHeaderView.heightAnchor.constraint(equalToConstant: height) ])
+        }
 
-        tableView.reloadData()
+        subHeaderView.backgroundColor = .white
+
+        addChildViewController(currencyList, layout: {
+            currencyList.view.constrain([
+                currencyList.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                currencyList.view.topAnchor.constraint(equalTo: subHeaderView.bottomAnchor),
+                currencyList.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                currencyList.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+        })
+        currencyList.didSelectCurrency = {
+            self.didSelectCurrency?($0)
+        }
+        subHeaderView.clipsToBounds = false
+        subHeaderView.addSubview(logo)
+        logo.constrain([
+            logo.leadingAnchor.constraint(equalTo: subHeaderView.leadingAnchor, constant: C.padding[2]),
+            logo.bottomAnchor.constraint(equalTo: subHeaderView.bottomAnchor, constant: -C.padding[2]),
+            logo.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25),
+            logo.heightAnchor.constraint(equalTo: logo.widthAnchor, multiplier: 230.0/772.0)])
+
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.shadowImage = #imageLiteral(resourceName: "TransparentPixel")
+        navigationController?.navigationBar.setBackgroundImage(#imageLiteral(resourceName: "TransparentPixel"), for: .default)
 
         stores[0].lazySubscribe(self, selector: { $0.walletState.balance != $1.walletState.balance }, callback: { _ in
             self.updateTotalAssets()
-            self.tableView.beginUpdates()
-            self.tableView.reloadRows(at: [IndexPath(row: 0, section: 0)], with: .automatic)
-            self.tableView.endUpdates()
         })
 
-        var i = 0
         stores.forEach {
-            let j = i
             $0.lazySubscribe(self, selector: { $0.walletState.bigBalance?.getString(10) != $1.walletState.bigBalance?.getString(10) }, callback: { _ in
                 self.updateTotalAssets()
-                self.tableView.beginUpdates()
-                self.tableView.reloadRows(at: [IndexPath(row: j, section: 0)], with: .automatic)
-                self.tableView.endUpdates()
             })
-
-            if $0.state.walletState.crowdsale != nil {
-                $0.lazySubscribe(self, selector: { $0.walletState.crowdsale != $1.walletState.crowdsale }, callback: { _  in
-                    self.tableView.beginUpdates()
-                    self.tableView.reloadRows(at: [IndexPath(row: j, section: 0)], with: .automatic)
-                    self.tableView.endUpdates()
-                })
-            }
-            i = i + 1
         }
+
+        addSubviews()
+        addConstraints()
+        setInitialData()
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    private func addSubviews() {
+        subHeaderView.addSubview(totalHeader)
+        subHeaderView.addSubview(total)
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currencies.count
+    private func addConstraints() {
+        total.constrain([
+            total.trailingAnchor.constraint(equalTo: subHeaderView.trailingAnchor, constant: -C.padding[2]),
+            total.bottomAnchor.constraint(equalTo: subHeaderView.bottomAnchor, constant: -C.padding[2]) ])
+        totalHeader.constrain([
+            totalHeader.trailingAnchor.constraint(equalTo: total.trailingAnchor),
+            totalHeader.bottomAnchor.constraint(equalTo: total.topAnchor, constant: 0.0) ])
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let store = stores[indexPath.row]
-        if store.state.walletState.token?.code == "BRD" {
-            let cell = tableView.dequeueReusableCell(withIdentifier: crowdSaleCellIdentifier, for: indexPath) as! CrowsaleCell
-            if let rate = store.state.currentRate {
-                let price = "$\(rate.rate)"
-                cell.setData(currencyName: currencies[indexPath.row].0, price: price, balance: balanceString(forStore: store), store: store)
-            }
-            return cell
-        } else {
-            let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! HomeScreenCell
-            if let rate = store.state.currentRate {
-                let price = "$\(rate.rate)"
-                cell.setData(currencyName: currencies[indexPath.row].0, price: price, balance: balanceString(forStore: store), store: store)
-            }
-            return cell
-        }
+    private func setInitialData() {
+        totalHeader.text = "total assets"
+        totalHeader.textAlignment = .left
+        total.textAlignment = .left
+        total.text = "$0"
+        title = "Assets"
+        navigationItem.titleView = UIView()
     }
 
     private func updateTotalAssets() {
@@ -112,34 +118,7 @@ class HomeScreenViewController : UITableViewController, Subscriber {
         format.generatesDecimalNumbers = true
         format.negativeFormat = format.positiveFormat.replacingCharacters(in: format.positiveFormat.range(of: "#")!, with: "-#")
         format.currencySymbol = stores[0].state.currentRate!.currencySymbol
-        let formattedString = format.string(from: NSNumber(value: total)) ?? ""
-        let label = UILabel()
-        label.text = "Total Assets: \(formattedString)"
-        navigationItem.titleView = label
-
-    }
-
-    private func balanceString(forStore store: Store) -> String {
-        if !store.isEthLike {
-            guard let balance = store.state.walletState.balance else { return "" }
-            return DisplayAmount(amount: Satoshis(rawValue: balance), state: store.state, selectedRate: nil, minimumFractionDigits: store.state.maxDigits, store: store).combinedDescription
-        } else {
-            guard let bigBalance = store.state.walletState.bigBalance else { return "" }
-            if store.state.currency == .ethereum {
-                return DisplayAmount.ethString(value: bigBalance, store: store) + " " + DisplayAmount.localEthString(value: bigBalance, store: store)
-            } else {
-                guard let token = store.state.walletState.token else { return "" }
-                var decimal = Decimal(string: bigBalance.getString(10)) ?? Decimal(0)
-                var amount: Decimal = 0.0
-                NSDecimalMultiplyByPowerOf10(&amount, &decimal, Int16(-1*token.decimals), .up)
-                let value = NSDecimalNumber(decimal: amount)
-                return String(describing: value) + " \(token.code)"
-            }
-        }
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        didSelectCurrency?(currencies[indexPath.row].1)
+        self.total.text = format.string(from: NSNumber(value: total))
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -147,20 +126,3 @@ class HomeScreenViewController : UITableViewController, Subscriber {
     }
 }
 
-extension HomeScreenViewController : UINavigationControllerDelegate {
-    func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-        if viewController is HomeScreenViewController {
-            navigationController.isNavigationBarHidden = false
-        } else {
-            navigationController.isNavigationBarHidden = true
-        }
-        navigationController.interactivePopGestureRecognizer?.isEnabled = true
-        navigationController.interactivePopGestureRecognizer?.delegate = self
-    }
-}
-
-extension HomeScreenViewController : UIGestureRecognizerDelegate {
-    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
-    }
-}
