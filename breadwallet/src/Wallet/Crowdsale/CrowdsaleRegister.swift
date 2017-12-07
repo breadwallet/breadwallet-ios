438d7643403bd87d3ed6cd4cc6a7292d8d16676b
Adrian Corscadden <me@acorscadden.com>
2017-12-07 12:01:29 -0800

    Add token

---
+++ breadwallet/src/Wallet/Crowdsale/CrowdsaleRegister.swift
@@ -0,0 +1,9 @@
+//
+//  CrowdsaleRegister.swift
+//  breadwallet
+//
+//  Created by Adrian Corscadden on 2017-12-07.
+//  Copyright © 2017 breadwallet LLC. All rights reserved.
+//
+
+import Foundation
