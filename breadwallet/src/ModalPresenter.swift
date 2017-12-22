2783af280d87ac086459b8271aac02a40ae8a8f0
losh11 <loshan1212@gmail.com>
2017-12-22 03:33:38 +0000

    Fix: support endpoint

--- breadwallet/src/ModalPresenter.swift
+++ breadwallet/src/ModalPresenter.swift
@@ -195,7 +195,7 @@ class ModalPresenter : Subscriber, Trackable {
         supportCenter.modalPresentationStyle = .overFullScreen
         supportCenter.modalPresentationCapturesStatusBarAppearance = true
         supportCenter.transitioningDelegate = supportCenter
-        let url = articleId == nil ? "/support" : "/support/article?id=\(articleId!)"
+        let url = articleId == nil ? "/support" : "/support?id=\(articleId!)"
         supportCenter.navigate(to: url)
         topViewController?.present(supportCenter, animated: true, completion: {})
     }
