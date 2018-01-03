bc1cffc6ebfc0c627c1c37f6f0c4f8ced7f849c9
Adrian Corscadden <me@acorscadden.com>
2018-01-02 22:31:21 -0800

    Fix 4 digit pin entry

--- breadwallet/src/ViewControllers/RootNavigationController.swift
+++ breadwallet/src/ViewControllers/RootNavigationController.swift
@@ -10,25 +10,10 @@ import UIKit
 
 class RootNavigationController : UINavigationController {
 
-    init(store: Store, rootViewController: UIViewController) {
-        self.store = store
-        self.tempLoginView = LoginViewController(store: store, isPresentedForLock: false)
-        super.init(rootViewController: rootViewController)
-    }
-
-    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        self.store = Store()
-        self.tempLoginView = LoginViewController(store: Store(), isPresentedForLock: false)
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     var walletManager: WalletManager? {
         didSet {
             guard let walletManager = walletManager else { return }
+            guard let store = store else { return }
             if !walletManager.noWallet {
                 let loginView = LoginViewController(store: store, isPresentedForLock: false, walletManager: walletManager)
                 loginView.transitioningDelegate = loginTransitionDelegate
@@ -36,26 +21,34 @@ class RootNavigationController : UINavigationController {
                 loginView.modalPresentationCapturesStatusBarAppearance = true
                 loginView.shouldSelfDismiss = true
                 present(loginView, animated: false, completion: {
-                    self.tempLoginView.remove()
+                    self.tempLoginView?.remove()
                     //todo - attempt show welcome here
                 })
             }
         }
     }
 
-    private let tempLoginView: LoginViewController
-    private let store: Store
+    var store: Store? {
+        didSet {
+            guard let store = store else { return }
+            self.tempLoginView = LoginViewController(store: store, isPresentedForLock: false)
+        }
+    }
+    private var tempLoginView: LoginViewController?
     private let welcomeTransitingDelegate = PinTransitioningDelegate()
     private let loginTransitionDelegate = LoginTransitionDelegate()
 
     override func viewDidLoad() {
         guardProtected(queue: DispatchQueue.main) {
             if !WalletManager.staticNoWallet {
-                self.addChildViewController(self.tempLoginView, layout: {
-                    self.tempLoginView.view.constrain(toSuperviewEdges: nil)
-                })
+                if let tempLoginView = self.tempLoginView {
+                    self.addChildViewController(tempLoginView, layout: {
+                        tempLoginView.view.constrain(toSuperviewEdges: nil)
+                    })
+                }
             } else {
-                let tempStartView = StartViewController(store: self.store, didTapCreate: {}, didTapRecover: {})
+                guard let store = self.store else { return }
+                let tempStartView = StartViewController(store: store, didTapCreate: {}, didTapRecover: {})
                 self.addChildViewController(tempStartView, layout: {
                     tempStartView.view.constrain(toSuperviewEdges: nil)
                     tempStartView.view.isUserInteractionEnabled = false
