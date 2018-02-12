cc16e8e10598886a2b412cfef0e3c998e637b00f
ehsan <e@ewerx.ca>
2018-02-12 17:14:23 -0500

    fix unit test compile errors (still failing)

--- breadwalletTests/WalletInfoTest.swift
+++ breadwalletTests/WalletInfoTest.swift
@@ -17,7 +17,7 @@ class WalletInfoTest : XCTestCase {
     override class func setUp() {
         clearKeychain()
         deleteDb()
-        walletManager = try! WalletManager(store: Store(), dbPath: nil)
+        walletManager = try! WalletManager(currency: Currencies.btc, dbPath: nil)
         let _ = walletManager?.setRandomSeedPhrase()
         client = walletManager?.apiClient
     }
