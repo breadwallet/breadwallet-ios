a68620ffaa2f6f218b816238dad6b1a9d42ef94e
Ehsan <e@ewerx.ca>
2018-02-22 17:51:20 -0500

    sync indicator and back button UI tweaks

--- breadwallet/src/Extensions/UIViewController+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIViewController+BRWAdditions.swift
@@ -32,4 +32,16 @@ extension UIViewController {
         }
         navigationItem.leftBarButtonItems = [UIBarButtonItem.negativePadding, UIBarButtonItem(customView: close)]
     }
+    
+    func addCustomBackButton() {
+        let backButton = UIButton(type:.system)
+        backButton.setImage(#imageLiteral(resourceName: "LeftArrow"), for: .normal)
+        backButton.frame = CGRect(x: 0, y: 0, width: 48, height: 48)
+        backButton.imageEdgeInsets = UIEdgeInsets(top: 12.0, left: 0.0, bottom: 12.0, right: 24.0)
+        backButton.tap = {
+            self.navigationController?.popViewController(animated: true)
+        }
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
+        navigationItem.hidesBackButton = true
+    }
 }
