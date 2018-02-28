01251c12e4a50906b659264f5fc0379f92d11d84
Adrian Corscadden <me@acorscadden.com>
2018-02-27 20:29:14 -0800

    Fix wrong info in confirmed in block. Fixes iOS-458

--- breadwallet/src/Constants/Constants.swift
+++ breadwallet/src/Constants/Constants.swift
@@ -46,4 +46,5 @@ struct C {
     static let feeCacheTimeout: TimeInterval = C.secondsInDay*3
     static let bCashForkBlockHeight: UInt32 = E.isTestnet ? 1155876 : 478559
     static let bCashForkTimeStamp: TimeInterval = E.isTestnet ? (1501597117 - NSTimeIntervalSince1970) : (1501568580 - NSTimeIntervalSince1970)
+    static let txUnconfirmedHeight = Int32.max
 }
