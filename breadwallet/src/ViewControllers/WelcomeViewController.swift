19531275a1180d6211482e4c0b2be1fe5ed2cb7a
ehsan <e@ewerx.ca>
2018-03-09 12:11:02 -0500

    add support button to BCH welcome modal, fixes IOS-523

--- breadwallet/src/ViewControllers/WelcomeViewController.swift
+++ breadwallet/src/ViewControllers/WelcomeViewController.swift
@@ -17,8 +17,9 @@ class WelcomeViewController : UIViewController, ContentBoxPresenter {
     private let header = GradientView()
     private let titleLabel = UILabel.wrapping(font: .customBold(size: 16.0), color: .darkText)
     private let body = UILabel.wrapping(font: .customBody(size: 16.0), color: .darkText)
-    private let buttonAction = ShadowButton(title: S.Button.home, type: .primary)
-    private let buttonDismiss = ShadowButton(title: S.Button.dismiss, type: .tertiary)
+    private let actionButton = ShadowButton(title: S.Button.home, type: .primary)
+    private let dismissButton = ShadowButton(title: S.Button.dismiss, type: .tertiary)
+    private let supportButton = UIButton.buildFaqButton(articleId: "bitcoin-cash")
 
     override func viewDidLoad() {
         addSubviews()
@@ -31,8 +32,9 @@ class WelcomeViewController : UIViewController, ContentBoxPresenter {
         contentBox.addSubview(header)
         contentBox.addSubview(titleLabel)
         contentBox.addSubview(body)
-        contentBox.addSubview(buttonAction)
-        contentBox.addSubview(buttonDismiss)
+        contentBox.addSubview(actionButton)
+        contentBox.addSubview(dismissButton)
+        header.addSubview(supportButton)
     }
 
     private func addConstraints() {
@@ -41,6 +43,11 @@ class WelcomeViewController : UIViewController, ContentBoxPresenter {
             contentBox.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             contentBox.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -C.padding[6] ) ])
         header.constrainTopCorners(height: 44.0)
+        supportButton.constrain([
+            supportButton.trailingAnchor.constraint(equalTo: header.trailingAnchor),
+            supportButton.centerYAnchor.constraint(equalTo: header.centerYAnchor),
+            supportButton.heightAnchor.constraint(equalToConstant: 44.0),
+            supportButton.widthAnchor.constraint(equalToConstant: 44.0) ])
         titleLabel.constrain([
             titleLabel.leadingAnchor.constraint(equalTo: contentBox.leadingAnchor, constant: C.padding[2]),
             titleLabel.topAnchor.constraint(equalTo: header.bottomAnchor, constant: C.padding[2]),
@@ -51,30 +58,31 @@ class WelcomeViewController : UIViewController, ContentBoxPresenter {
             body.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: C.padding[2]),
             body.trailingAnchor.constraint(equalTo: contentBox.trailingAnchor, constant: -C.padding[2]) ])
 
-        buttonDismiss.constrain([
-            buttonDismiss.leadingAnchor.constraint(equalTo: body.leadingAnchor),
-            buttonDismiss.topAnchor.constraint(equalTo: body.bottomAnchor, constant: C.padding[2]),
-            buttonDismiss.bottomAnchor.constraint(equalTo: contentBox.bottomAnchor, constant: -C.padding[2]),
-            buttonDismiss.widthAnchor.constraint(equalTo: buttonAction.widthAnchor)
+        dismissButton.constrain([
+            dismissButton.leadingAnchor.constraint(equalTo: body.leadingAnchor),
+            dismissButton.topAnchor.constraint(equalTo: body.bottomAnchor, constant: C.padding[2]),
+            dismissButton.bottomAnchor.constraint(equalTo: contentBox.bottomAnchor, constant: -C.padding[2]),
+            dismissButton.widthAnchor.constraint(equalTo: actionButton.widthAnchor)
             ])
         
-        buttonAction.constrain([
-            buttonAction.leadingAnchor.constraint(equalTo: buttonDismiss.trailingAnchor, constant: C.padding[2]),
-            buttonAction.topAnchor.constraint(equalTo: body.bottomAnchor, constant: C.padding[2]),
-            buttonAction.trailingAnchor.constraint(equalTo: body.trailingAnchor),
-            buttonAction.bottomAnchor.constraint(equalTo: contentBox.bottomAnchor, constant: -C.padding[2]) ])
+        actionButton.constrain([
+            actionButton.leadingAnchor.constraint(equalTo: dismissButton.trailingAnchor, constant: C.padding[2]),
+            actionButton.topAnchor.constraint(equalTo: body.bottomAnchor, constant: C.padding[2]),
+            actionButton.trailingAnchor.constraint(equalTo: body.trailingAnchor),
+            actionButton.bottomAnchor.constraint(equalTo: contentBox.bottomAnchor, constant: -C.padding[2]) ])
     }
 
     private func setInitialData() {
         view.backgroundColor = .clear
         contentBox.layer.cornerRadius = 6.0
         contentBox.layer.masksToBounds = true
+        supportButton.tintColor = .white
         titleLabel.text = S.Welcome.title
         body.text = S.Welcome.body
-        buttonDismiss.tap = { [unowned self] in
+        dismissButton.tap = { [unowned self] in
             self.dismiss(animated: true, completion: nil)
         }
-        buttonAction.tap = { [unowned self] in
+        actionButton.tap = { [unowned self] in
             let nc = self.presentingViewController as? RootNavigationController
             self.dismiss(animated: true, completion: {
                 nc?.popToRootViewController(animated: true)
