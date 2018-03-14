d08f8fbf34aa027e9fbb171d7bb44b8c28ea04d6
Ed Gamble <ed@breadwallet.com>
2018-03-14 14:29:15 -0700

    IOS-443: Use breadwallet-core feature/IOS-443 for Ethereum; create an Xcode EthereumCoreTests project

---
+++ BRCoreEthereumTests/BRCoreEthereumTests.swift
@@ -0,0 +1,37 @@
+//
+//  BRCoreEthereumTests.swift
+//  BRCoreEthereumTests
+//
+//  Created by Ed Gamble on 3/13/18.
+//  Copyright © 2018 breadwallet LLC. All rights reserved.
+//
+
+import XCTest
+//@testable import brcore
+
+class BRCoreEthereumTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+      runTests()
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
