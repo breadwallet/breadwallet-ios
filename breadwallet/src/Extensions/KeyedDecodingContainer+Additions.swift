4c4bd3be3664666b27ad3d437fb205d0d4332ce2
ehsan <e@ewerx.ca>
2018-04-03 20:14:25 -0400

    improved json parsing error output

--- breadwallet/src/Extensions/KeyedDecodingContainer+Additions.swift
+++ breadwallet/src/Extensions/KeyedDecodingContainer+Additions.swift
@@ -12,7 +12,7 @@ public extension KeyedDecodingContainer {
     public func decodeFromString<T: LosslessStringConvertible>(_ type: T.Type, forKey key: Key) throws -> T {
         let stringValue = try self.decode(String.self, forKey: key)
         guard let value = T(stringValue) else {
-            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Could not parse JSON string to a typed object")
+            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Could not parse JSON string (\(stringValue)) to a typed object (\(key.stringValue): \(String(describing: T.self)))")
             throw DecodingError.dataCorrupted(context)
         }
         return value
