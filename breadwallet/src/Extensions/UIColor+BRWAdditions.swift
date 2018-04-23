8506f7dfaa9a98ce4ee17f05985290f80a2dac04
ehsan <e@ewerx.ca>
2018-04-23 14:01:00 -0400

    add colors to platform /_wallet/currencies response

--- breadwallet/src/Extensions/UIColor+BRWAdditions.swift
+++ breadwallet/src/Extensions/UIColor+BRWAdditions.swift
@@ -188,4 +188,14 @@ extension UIColor {
             blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
             alpha: CGFloat(1.0))
     }
+    
+    var toHex: String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
 }
