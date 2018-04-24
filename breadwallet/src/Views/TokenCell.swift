8d2d24de46eafb5b208b826ab7a0605f6da248f5
Adrian Corscadden <me@acorscadden.com>
2018-04-24 14:04:29 -0700

    Add token icons

--- breadwallet/src/Views/TokenCell.swift
+++ breadwallet/src/Views/TokenCell.swift
@@ -13,12 +13,10 @@ class TokenCell : UITableViewCell {
     private let header = UILabel(font: .customBold(size: 18.0), color: .darkText)
     private let subheader = UILabel(font: .customBody(size: 16.0), color: .secondaryShadow)
     private let icon = UIImageView()
-    private let iconBackground = UIView()
     private let button = UIButton.outline(title: S.TokenList.add)
     private var address: String = ""
     private var listType: TokenListType = .add
     private var isTokenHidden = false
-    private var tokenIconLabel = UILabel.init(font: .customBold(size: 18.0), color: .white)
 
     var didAddToken:((String)->Void)?
     var didRemoveToken:((String)->Void)?
@@ -34,8 +32,7 @@ class TokenCell : UITableViewCell {
         self.address = token.address
         self.listType = listType
         self.isTokenHidden = token.isHidden
-        iconBackground.backgroundColor = UIColor.fromHex(token.colors[0])
-        tokenIconLabel.text = String(token.name[...token.name.startIndex])
+        self.icon.image = UIImage(named: token.code.lowercased())
         setInitialButtonState()
     }
 
@@ -48,26 +45,22 @@ class TokenCell : UITableViewCell {
     private func addSubviews() {
         contentView.addSubview(header)
         contentView.addSubview(subheader)
-        contentView.addSubview(iconBackground)
-        iconBackground.addSubview(icon)
+        contentView.addSubview(icon)
         contentView.addSubview(button)
-        iconBackground.addSubview(tokenIconLabel)
     }
 
     private func addConstraints() {
-        iconBackground.constrain([
-            iconBackground.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: C.padding[1]),
-            iconBackground.topAnchor.constraint(equalTo: contentView.topAnchor, constant: C.padding[1]),
-            iconBackground.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -C.padding[1]),
-            iconBackground.heightAnchor.constraint(equalToConstant: 44.0),
-            iconBackground.widthAnchor.constraint(equalToConstant: 44.0)])
-        icon.constrain(toSuperviewEdges: UIEdgeInsetsMake(C.padding[1], C.padding[1]+2, -C.padding[1], -C.padding[1]))
-        tokenIconLabel.constrain(toSuperviewEdges: UIEdgeInsetsMake(C.padding[1], C.padding[1]+2, -C.padding[1], -C.padding[1]))
+        icon.constrain([
+            icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: C.padding[1]),
+            icon.topAnchor.constraint(equalTo: contentView.topAnchor, constant: C.padding[1]),
+            icon.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -C.padding[1]),
+            icon.heightAnchor.constraint(equalToConstant: 44.0),
+            icon.widthAnchor.constraint(equalToConstant: 44.0)])
         header.constrain([
-            header.leadingAnchor.constraint(equalTo: iconBackground.trailingAnchor, constant: C.padding[1]),
+            header.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: C.padding[1]),
             header.bottomAnchor.constraint(equalTo: contentView.centerYAnchor)])
         subheader.constrain([
-            subheader.leadingAnchor.constraint(equalTo: iconBackground.trailingAnchor, constant: C.padding[1]),
+            subheader.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: C.padding[1]),
             subheader.topAnchor.constraint(equalTo: contentView.centerYAnchor)])
         button.constrain([
             button.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -C.padding[1]),
@@ -79,11 +72,7 @@ class TokenCell : UITableViewCell {
     private func setInitialData() {
         selectionStyle = .none
         icon.contentMode = .scaleAspectFill
-        iconBackground.backgroundColor = .blue
-        iconBackground.layer.cornerRadius = 4.0
-        iconBackground.layer.masksToBounds = true
         setInitialButtonState()
-        tokenIconLabel.textAlignment = .center
     }
     
     private func setInitialButtonState() {
