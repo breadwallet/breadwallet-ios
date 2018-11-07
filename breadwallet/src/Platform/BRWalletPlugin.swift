7918ba44b9a21f6f74af460538472feeed0deb7b
Ehsan <ehsan@brd.com>
2018-11-07 13:50:16 -0500

    send legacy btc address to platform [IOS-926]

--- breadwallet/src/Platform/BRWalletPlugin.swift
+++ breadwallet/src/Platform/BRWalletPlugin.swift
@@ -297,8 +297,11 @@ class BRWalletPlugin: BRHTTPRouterPlugin, BRWebSocketClient, Trackable {
             var response = [[String: Any]]()
             let currencies = Store.state.currencies.filter { code == nil || $0.code.lowercased() == code!.lowercased() }
             for currency in currencies {
-                response.append(["currency": currency.code,
-                                 "address": Store.state[currency]?.receiveAddress ?? ""])
+                // prefer legacy addresses for now until platforms support segwit
+                if let address = Store.state[currency]?.legacyReceiveAddress ?? Store.state[currency]?.receiveAddress {
+                    response.append(["currency": currency.code,
+                                     "address": address])
+                }
             }
             return try BRHTTPResponse(request: req, code: 200, json: (response.count == 1) ? response.first! : response)
         }
@@ -496,7 +499,7 @@ extension BRWalletPlugin {
         guard let walletManager = btcWalletManager else { return d }
         d["no_wallet"] = walletManager.noWallet
         if let wallet = walletManager.wallet {
-            d["receive_address"] = wallet.receiveAddress
+            d["receive_address"] = wallet.legacyReceiveAddress ?? wallet.receiveAddress
             //d["watch_only"] = TODO - add watch only
         }
         d["btc_denomination_digits"] = walletManager.currency.state?.maxDigits
