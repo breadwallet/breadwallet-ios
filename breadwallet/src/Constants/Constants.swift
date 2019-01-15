a073b486d8470e58b9492fdf88b2e58b7f9d3151
ray vander veen <ray.vanderveen@brd.com>
2019-01-15 15:56:43 -0500

    [IOS-993] Include current and previous console logs with Send Logs
    **Background**
    
    The developer menu option Send Logs was only capturing the console output from the current instantiation
    of the app, because whenever the app started up it would reopen and overwrite the contents of log.txt.
    
    This meant that if the app were to crash and we asked a user to send logs, we'd potentially miss clues
    from the previous run of the app in which it crashed.
    
    **Solution*
    
    - on startup, check for content in log.txt and save this to previouslog.txt before overwriting log.txt
    - when attaching the logs to the mail composer in `MessageUIPresenter`, attach both the current and previous logs
    
    **To Test**
    
    - run the app, hit Menu / Developer / Send Logs
    - there should be one attachment for the current log
    - shut down and restart the app, go back to Send Logs, and you should see two attachments
    - change the To: field to send to your own email, and examine the attachments to verify
      that they have different content
    - restart the app again and verify that you can send two new attachments; the content of the
      log marked *previous* should match what was in the log marked *current* (except possibly
      a bit at the end of the logs -- I believe this is due to some variability in when the console
      logging is actually flushed to log.txt)

--- breadwallet/src/Constants/Constants.swift
+++ breadwallet/src/Constants/Constants.swift
@@ -50,10 +50,22 @@ struct C {
     static let bCashForkBlockHeight: UInt32 = E.isTestnet ? 1155876 : 478559
     static let bCashForkTimeStamp: TimeInterval = E.isTestnet ? (1501597117 - NSTimeIntervalSince1970) : (1501568580 - NSTimeIntervalSince1970)
     static let txUnconfirmedHeight = Int32.max
+    
+    static let consoleLogFileName = "log.txt"
+    static let previousConsoleLogFileName = "previouslog.txt"
+    
+    // Returns the console log file path for the current instantiation of the app.
     static var logFilePath: URL {
         let cachesDirectory = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!
-        return URL(fileURLWithPath: cachesDirectory).appendingPathComponent("log.txt")
+        return URL(fileURLWithPath: cachesDirectory).appendingPathComponent(consoleLogFileName)
     }
+    
+    // Returns the console log file path for the previous instantiation of the app.
+    static var previousLogFilePath: URL {
+        let cachesDirectory = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first!
+        return URL(fileURLWithPath: cachesDirectory).appendingPathComponent(previousConsoleLogFileName)
+    }
+    
     static let usdCurrencyCode = "USD"
     static let euroCurrencyCode = "EUR"
     static let britishPoundCurrencyCode = "GBP"
