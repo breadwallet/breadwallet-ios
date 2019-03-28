fac91823a7f25afddc7fc61fd38bc654444b6749
Ehsan <ehsan@brd.com>
2019-03-28 14:23:02 +0000

    update to Xcode 10.2 / Swift 5

--- breadwallet/ERC20ABI.swift
+++ breadwallet/ERC20ABI.swift
@@ -9,9 +9,9 @@
 import Foundation
 
 public extension ERC20Token {
-    public static let transferEventSignature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+    static let transferEventSignature = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
     
-    public static let standardAbi = """
+    static let standardAbi = """
 [
   {
     "constant": true,
--- breadwallet/src/PaymentProtocol.swift
+++ breadwallet/src/PaymentProtocol.swift
@@ -244,7 +244,7 @@ class PaymentProtocolRequest {
             var trustResult = SecTrustResultType.invalid
             
             for c in self.certs {
-                if let cert = SecCertificateCreateWithData(nil, Data(bytes: c) as CFData) { certs.append(cert) }
+                if let cert = SecCertificateCreateWithData(nil, Data(c) as CFData) { certs.append(cert) }
             }
             
             if !certs.isEmpty {
