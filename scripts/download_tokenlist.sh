400678021bbb4455dbfb18f5d937352bfe05f92c
Ehsan <ehsan@brd.com>
2019-05-02 19:02:56 +0000

    [IOS-1106] remove asset bundle download build phase, update manual scripts

--- scripts/download_tokenlist.sh
+++ scripts/download_tokenlist.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Downlaods the latest currencies list to be embedded
 filename="tokens.json"
-host="stage2.breadwallet.com"
-if [[ "${CONFIGURATION}" == "Release" || ! "${CONFIGURATION}" ]]; then
-  host="api.breadwallet.com"
-fi
+host="api.breadwallet.com"
 echo "Downloading ${filename} from ${host}..."
 curl --silent --show-error --output "breadwallet/Resources/${filename}" https://${host}/currencies?type=erc20
