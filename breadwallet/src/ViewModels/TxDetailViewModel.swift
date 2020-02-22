4dcd4c6b525ecfad57491b15214dfc7d88d16bf5
Adrian Corscadden <me@acorscadden.com>
2020-02-22 10:37:36 -0800

    Remove 0x prefix from non eth txn IDs

--- breadwallet/src/ViewModels/TxDetailViewModel.swift
+++ breadwallet/src/ViewModels/TxDetailViewModel.swift
@@ -17,7 +17,6 @@ struct TxDetailViewModel: TxViewModel {
     let fiatAmount: String
     let originalFiatAmount: String?
     let exchangeRate: String?
-    let transactionHash: String
     let tx: Transaction
     
     // Ethereum-specific fields
@@ -68,6 +67,10 @@ struct TxDetailViewModel: TxViewModel {
             }
         }
     }
+    
+    var transactionHash: String {
+        return currency.isEthereumCompatible ? tx.hash : tx.hash.removing(prefix: "0x")
+    }
 }
 
 extension TxDetailViewModel {
@@ -79,7 +82,6 @@ extension TxDetailViewModel {
         fiatAmount = fiatAmounts.0
         originalFiatAmount = fiatAmounts.1
         exchangeRate = TxDetailViewModel.exchangeRateText(tx: tx)
-        transactionHash = tx.hash
         self.tx = tx
 
         if tx.direction == .sent {
