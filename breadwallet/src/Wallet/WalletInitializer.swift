98297ebe411e2f4bbbb35f2f1b3192ec740be14e
Adrian Corscadden <me@acorscadden.com>
2020-04-15 11:12:42 -0700

    Update core and fix warnings

--- breadwallet/src/Wallet/WalletInitializer.swift
+++ breadwallet/src/Wallet/WalletInitializer.swift
@@ -14,27 +14,27 @@ import WalletKit
 extension CoreSystem {
     
     func initialize(network: Network, system: System, callback: @escaping (Data?) -> Void) {
-        system.accountInitialize (system.account, onNetwork: network, createIfDoesNotExist: false) { (res: Result<Data, System.AccountInitializationError>) in
+        system.accountInitialize(system.account, onNetwork: network, createIfDoesNotExist: false) { (res: Result<Data, System.AccountInitializationError>) in
             var serializationData: Data?
             switch res {
             case .success (let data):
-                print ("[SYS] system : success : \(data)")
+                print("[SYS] system : success : \(data)")
                 serializationData = data
             case .failure (let error):
                 switch error {
                 case .alreadyInitialized:
-                    print ("[SYS] system : Already Initialized")
+                    print("[SYS] system : Already Initialized")
                 case .multipleHederaAccounts(let accounts):
-                    print ("[SYS] system: Multiple Hedera Accounts: \(accounts)")
+                    print("[SYS] system: Multiple Hedera Accounts: \(accounts)")
                     let hederaAccount = accounts.sorted { ($0.balance ?? 0) > ($1.balance ?? 0) }[0]
-                    serializationData = system.accountInitialize (system.account,
+                    serializationData = system.accountInitialize(system.account,
                                                                   onNetwork: network,
                                                                   hedera: hederaAccount)
                 case .queryFailure(let message):
-                    print ("[SYS] system: Initalization Query Error: \(message)")
+                    print("[SYS] system: Initalization Query Error: \(message)")
                 // Account doesn't exists
                 case .cantCreate:
-                    print ("[SYS] system: Initializaiton: Can't Create")
+                    print("[SYS] system: Initializaiton: Can't Create")
                 }
             }
             callback(serializationData)
