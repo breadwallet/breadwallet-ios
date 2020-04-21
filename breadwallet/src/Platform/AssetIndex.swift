66f629c6ac19d95d784b2136d25de82381d624e5
Adrian Corscadden <me@acorscadden.com>
2020-04-21 13:55:14 -0700

    MR feedback

--- breadwallet/src/Platform/AssetIndex.swift
+++ breadwallet/src/Platform/AssetIndex.swift
@@ -65,10 +65,11 @@ class AssetIndex: BRKVStoreObject, Codable {
     }
     
     static var defaultCurrencyIds: [CurrencyId] {
-        return Currencies.allCases.map { $0.uid }.filter { $0 != Currencies.bch.uid &&
-                $0 != Currencies.xrp.uid &&
-                $0 != Currencies.tusd.uid &&
-                $0 != Currencies.hbar.uid }
+        return [
+            Currencies.btc.uid,
+            Currencies.eth.uid,
+            Currencies.brd.uid
+        ]
     }
 }
 
