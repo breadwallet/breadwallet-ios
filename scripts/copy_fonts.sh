5332ae640e36f63334f9a858d2accc3b482157a1
Adrian Corscadden <me@acorscadden.com>
2020-07-23 10:07:40 -0700

    Remove font

---
+++ scripts/copy_fonts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Copying fonts...";
+
+copy_font() {
+	if cp "Modules/mobile-font/$1" "breadwallet/Fonts/$2"
+	then
+  		echo "Success $1"
+	else
+		cp "scripts/SF-Pro-Text-Regular.otf" "breadwallet/Fonts/$2"
+		echo "Used placeholder $1"
+	fi
+}
+
+copy_font "mobile_font_bold.otf" "Font-Bold.otf"
+copy_font "mobile_font_book.otf" "Font-Book.otf"
+copy_font "mobile_font_medium.otf" "Font-Medium.otf"
