1c297934262be8235d28f1cbf693b0cc66866e7b
Giancarlo Pacheco <g1an@hotmail.com>
2020-09-11 16:37:11 -0400

    Add code from BRD to avoid future conflicts

--- breadwallet/src/ViewControllers/HomeScreenViewController.swift
+++ breadwallet/src/ViewControllers/HomeScreenViewController.swift
@@ -226,6 +226,7 @@ class HomeScreenViewController: UIViewController, Subscriber, Trackable {
     
     private func setupToolbar() {
         let buttons = [("Redeem", #imageLiteral(resourceName: "buy"), #selector(redeem)),
+                       ("Scan QR", #imageLiteral(resourceName: "buy"), #selector(redeem)),
                        (S.HomeScreen.menu, #imageLiteral(resourceName: "menu"), #selector(menu))].map { (title, image, selector) -> UIBarButtonItem in
                         let button = UIButton.vertical(title: title, image: image)
                         button.tintColor = .navigationTint
@@ -322,6 +323,16 @@ class HomeScreenViewController: UIViewController, Subscriber, Trackable {
     }
     
     // MARK: Actions
+    
+    @objc private func buy() {
+        saveEvent("currency.didTapBuyBitcoin", attributes: [ "buyAndSell": shouldShowBuyAndSell ? "true" : "false" ])
+        didTapBuy?()
+    }
+    
+    @objc private func trade() {
+        saveEvent("currency.didTapTrade", attributes: [:])
+        didTapTrade?()
+    }
     
     @objc private func redeem() {
         saveEvent("currency.didTapRedemption", attributes: [:])
