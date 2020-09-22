94dabc5908f2f733d6e1f8321133c1fed8f185d4
Gardner von Holt <gardner@wvh.com>
2020-09-22 15:57:53 -0500

    fix if statement typo

--- scripts/uploadbuild.sh
+++ scripts/uploadbuild.sh
@@ -1,5 +1,5 @@
-if [[ $1 == releases* ]]
-  echo "Release build detected . Will upload to test flight.  Branch is " + $1
+if [[ $1 == releases* ]]; then
+  echo "Release build detected . Will upload to TestFlight.  Branch is " $1
   archive_path="$PWD/build/"$scheme".xcarchive"
   export_path="$PWD/build"
   scheme="breadwallet"
@@ -8,5 +8,5 @@ if [[ $1 == releases* ]]
   xcodebuild -exportArchive -archivePath "$archive_path" -exportOptionsPlist $PWD/build/exportOptions.plist -exportPath $PWD/build | $xcpretty
   xcrun altool --upload-app --type ios --file "$export_path/"$scheme".ipa" --username "$APPLE_BUILD_USER" --password "$APPLE_BUILD_PASSWORD"
 else
-  echo "Not a releases build.  Will NOT upload. Branch is " + $1
+  echo "Not a releases build.  Will NOT upload. Branch is " $1
 fi
