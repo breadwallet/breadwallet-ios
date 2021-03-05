7235b2f02800ce0ad3e6954999884146948110b2
Michael Inger <michael.inger@breadwallet.com>
2021-03-05 22:52:12 +0000

    fixing tests
    fixing tests

--- breadwallet/src/Wallet/Currency.swift
+++ breadwallet/src/Wallet/Currency.swift
@@ -493,6 +493,7 @@ enum Currencies: String, CaseIterable {
     case xrp
     case hbar
     case xtz
+    case usdc
     
     var code: String { return rawValue }
     var uid: CurrencyId {
@@ -514,6 +515,8 @@ enum Currencies: String, CaseIterable {
             uids = "hedera-mainnet:__native__"
         case .xtz:
             uids = "tezos-mainnet:__native__"
+        case .usdc:
+            uids = "ethereum-mainnet:0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
         }
         return CurrencyId(rawValue: uids)
     }
