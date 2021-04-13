12e4cd818788259c4f64d5055a91dbf58fb74a37
Giancarlo Pacheco <g1an@hotmail.com>
2021-04-13 09:44:45 -0400

    Fixing keychain attribute service that may have caused customers to get zero balances as it was accessing the wrong keychain item. Upgrading to this new version should recover the item, hence the balances for the user as the keychain does not get removed easily

--- breadwallet/KeyStore/KeyStoreUtils.swift
+++ breadwallet/KeyStore/KeyStoreUtils.swift
@@ -11,13 +11,13 @@
 import Foundation
 
 var WalletSecAttrService: String {
-    if E.isRunningTests { return "com.brd.testnetQA.tests" }
+    if E.isRunningTests { return "cash.just.testnetQA.tests" }
     #if TESTNET
-    return "com.brd.testnetQA"
+    return "cash.just.testnetQA"
     #elseif INTERNAL
-    return "com.brd.internalQA"
+    return "cash.just.internalQA"
     #else
-    return "org.voisine.breadwallet"
+    return "cash.just.breadwallet"
     #endif
 }
 
